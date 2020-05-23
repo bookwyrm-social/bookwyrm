d085071c4690bdac40ec968c41b61a7aa6a8cf31
Mouse Reeve <mousereeve@riseup.net>
2020-05-23 15:38:15 -0700

    Rename actor file

--- fedireads/activitypub/__init__.py
+++ fedireads/activitypub/__init__.py
@@ -1,6 +1,7 @@
 ''' bring activitypub functions into the namespace '''
 from .base_activity import ActivityEncoder, Image, PublicKey
-from .actor import Person
+from .person import Person
+
 from .book import get_book, get_author, get_shelf
 from .create import get_create, get_update
 from .follow import get_following, get_followers
