2d4dab00cc92466e9886bdb50bec2c95306cf8ef
Mouse Reeve <mousereeve@riseup.net>
2020-09-21 10:33:23 -0700

    Uncomments admin app

--- bookwyrm/settings.py
+++ bookwyrm/settings.py
@@ -31,7 +31,7 @@ OL_URL = env('OL_URL')
 # Application definition
 
 INSTALLED_APPS = [
-    #'django.contrib.admin',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
