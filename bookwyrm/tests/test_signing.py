e8ef8f710180e2cdf10075a23f57193f3dd9d916
Mouse Reeve <mousereeve@riseup.net>
2020-10-15 10:55:04 -0700

    Fixes data encoding for signing tests

--- bookwyrm/tests/test_signing.py
+++ bookwyrm/tests/test_signing.py
@@ -61,7 +61,7 @@ class Signature(TestCase):
             digest=None,
             date=None):
         now = date or http_date()
-        data = get_follow_data(sender, self.rat)
+        data = json.dumps(get_follow_data(sender, self.rat)).encode('utf-8')
         digest = digest or make_digest(data)
         signature = make_signature(
             signer or sender, self.rat.inbox, now, digest)
