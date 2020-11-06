ad25e752820c62cbb9412b7f2696a31adc194d94
Mouse Reeve <mousereeve@riseup.net>
2020-11-06 14:25:48 -0800

    fixes tagging

--- bookwyrm/urls.py
+++ bookwyrm/urls.py
@@ -75,7 +75,7 @@ urlpatterns = [
     # books
     re_path(r'%s(.json)?/?$' % book_path, views.book_page),
     re_path(r'%s/edit/?$' % book_path, views.edit_book_page),
-    re_path(r'%s/editions(.json)?/?' % book_path, views.editions_page),
+    re_path(r'%s/editions(.json)?/?$' % book_path, views.editions_page),
 
     re_path(r'^author/(?P<author_id>[\w\-]+)(.json)?/?$', views.author_page),
     # TODO: tag needs a .json path
