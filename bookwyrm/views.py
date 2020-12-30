d3161ea3619677ba228839640d5427ae33d71a24
Mouse Reeve <mousereeve@riseup.net>
2020-12-30 11:42:07 -0800

    Send appropriate error codes back with error pages

--- bookwyrm/views.py
+++ bookwyrm/views.py
@@ -38,12 +38,14 @@ def is_api_request(request):
 
 def server_error_page(request):
     ''' 500 errors '''
-    return TemplateResponse(request, 'error.html', {'title': 'Oops!'})
+    return TemplateResponse(
+        request, 'error.html', {'title': 'Oops!'}, status=500)
 
 
 def not_found_page(request, _):
     ''' 404s '''
-    return TemplateResponse(request, 'notfound.html', {'title': 'Not found'})
+    return TemplateResponse(
+        request, 'notfound.html', {'title': 'Not found'}, status=404)
 
 
 @login_required
