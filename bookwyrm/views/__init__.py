ffd403dd47cfc323b98f054facd15e8625120aec
Mouse Reeve <mousereeve@riseup.net>
2021-01-13 12:08:35 -0800

    sorts imports for views

--- bookwyrm/views/__init__.py
+++ bookwyrm/views/__init__.py
@@ -1,19 +1,20 @@
 ''' make sure all our nice views are available '''
 from .authentication import Login, Register, Logout
-from .password import PasswordResetRequest, PasswordReset, ChangePassword
+from .author import Author, EditAuthor
+from .books import Book, EditBook, Editions
+from .books import upload_cover, add_description, switch_edition, resolve_book
+from .direct_message import DirectMessage
+from .error import not_found_page, server_error_page
+from .import_data import Import, ImportStatus
+from .interaction import Favorite, Unfavorite, Boost, Unboost
 from .invite import ManageInvites, Invite
 from .landing import About, Home, Feed, Discover
 from .notifications import Notifications
-from .direct_message import DirectMessage
-from .import_data import Import, ImportStatus
-from .user import User, EditUser, Followers, Following
-from .status import Status, Replies, CreateStatus, DeleteStatus
-from .interaction import Favorite, Unfavorite, Boost, Unboost
-from .books import Book, EditBook, Editions
-from .books import upload_cover, add_description, switch_edition, resolve_book
-from .author import Author, EditAuthor
+from .password import PasswordResetRequest, PasswordReset, ChangePassword
 from .tag import Tag, AddTag, RemoveTag
+from .search import Search
 from .shelf import Shelf
 from .shelf import user_shelves_page, create_shelf, delete_shelf
 from .shelf import shelve, unshelve
-from .search import Search
+from .status import Status, Replies, CreateStatus, DeleteStatus
+from .user import User, EditUser, Followers, Following
