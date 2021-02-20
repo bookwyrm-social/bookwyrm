b85fed359574039cfba25bd5ea969885109234eb
Henri Bourcereau <hbourcereau@atixnet.fr>
2021-02-20 17:02:36 +0100

    librarything import

---
+++ bookwyrm/librarything_import.py
@@ -0,0 +1,39 @@
+''' handle reading a csv from librarything '''
+import csv
+import re
+import math
+
+from bookwyrm import models
+from bookwyrm.models import ImportItem
+from bookwyrm.importer import Importer
+
+
+class LibrarythingImporter(Importer):
+    service = 'LibraryThing'
+    delimiter = '\t'
+    encoding = 'ISO-8859-1'
+    # mandatory_fields : fields matching the book ISBN13, title and author
+    mandatory_fields = ['ISBN', 'Title', 'Primary Author']
+
+    def parse_fields(self, initial):
+        data = {}
+        data['import_source']   = self.service
+        data['Book Id'] = initial['Book Id']
+        data['Title'] = initial['Title']
+        data['Author'] = initial['Primary Author']
+        data['ISBN13'] = initial['ISBN']
+        data['My Review'] = initial['Review']
+        data['My Rating'] = math.ceil(float(initial['Rating']))
+        data['Date Added'] = re.sub('\[|\]', '', initial['Entry Date'])
+        data['Date Started'] = re.sub('\[|\]', '', initial['Date Started'])
+        data['Date Read'] = re.sub('\[|\]', '', initial['Date Read'])
+
+        data['Exclusive Shelf'] = None
+        if data['Date Read']:
+            data['Exclusive Shelf'] = "read"
+        elif data['Date Started']:
+            data['Exclusive Shelf'] = "reading"
+        else:
+            data['Exclusive Shelf'] = "to-read"
+
+        return data
