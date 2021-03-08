5fe989e20f39f9edd4a3b3f7dee7de810c6d6d96
Mouse Reeve <mousereeve@riseup.net>
2021-03-08 09:58:07 -0800

    Formats migrationwq

--- bookwyrm/migrations/0048_merge_20210308_1754.py
+++ bookwyrm/migrations/0048_merge_20210308_1754.py
@@ -6,9 +6,8 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('bookwyrm', '0047_connector_isbn_search_url'),
-        ('bookwyrm', '0047_merge_20210228_1839'),
+        ("bookwyrm", "0047_connector_isbn_search_url"),
+        ("bookwyrm", "0047_merge_20210228_1839"),
     ]
 
-    operations = [
-    ]
+    operations = []
