1b5c71c7b1377754ce1378d7594bcc29ada9d55a
Mouse Reeve <mousereeve@riseup.net>
2021-03-19 12:44:30 -0700

    formatting

--- bookwyrm/migrations/0054_auto_20210319_1942.py
+++ bookwyrm/migrations/0054_auto_20210319_1942.py
@@ -6,18 +6,20 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('bookwyrm', '0053_auto_20210319_1913'),
+        ("bookwyrm", "0053_auto_20210319_1913"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='importitem',
-            name='data',
+            model_name="importitem",
+            name="data",
             field=models.JSONField(),
         ),
         migrations.AlterField(
-            model_name='user',
-            name='first_name',
-            field=models.CharField(blank=True, max_length=150, verbose_name='first name'),
+            model_name="user",
+            name="first_name",
+            field=models.CharField(
+                blank=True, max_length=150, verbose_name="first name"
+            ),
         ),
     ]
