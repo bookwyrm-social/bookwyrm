3be420944eee1bcacc80c89c22a2c7cf51c5dc7c
Mouse Reeve <mousereeve@riseup.net>
2021-03-29 13:51:06 -0700

    Fixes display of dms pagination

--- bookwyrm/views/feed.py
+++ bookwyrm/views/feed.py
@@ -87,7 +87,9 @@ class DirectMessage(View):
         if user:
             queryset = queryset.filter(Q(user=user) | Q(mention_users=user))
 
-        activities = privacy_filter(request.user, queryset, privacy_levels=["direct"])
+        activities = privacy_filter(
+            request.user, queryset, privacy_levels=["direct"]
+        ).order_by('-published_date')
 
         paginated = Paginator(activities, PAGE_LENGTH)
         activity_page = paginated.page(page)
