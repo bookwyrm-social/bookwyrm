bfdcf611e7966ea54d051a313115c40cf35eb40b
Mouse Reeve <mousereeve@riseup.net>
2021-04-06 13:54:20 -0700

    Adds inventaire identifier to book data fields

--- bookwyrm/migrations/0062_auto_20210406_1731.py
+++ bookwyrm/migrations/0062_auto_20210406_1731.py
@@ -6,17 +6,25 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('bookwyrm', '0061_auto_20210402_1435'),
+        ("bookwyrm", "0061_auto_20210402_1435"),
     ]
 
     operations = [
         migrations.RemoveConstraint(
-            model_name='connector',
-            name='connector_file_valid',
+            model_name="connector",
+            name="connector_file_valid",
         ),
         migrations.AlterField(
-            model_name='connector',
-            name='connector_file',
-            field=models.CharField(choices=[('openlibrary', 'Openlibrary'), ('inventaire', 'Inventaire'), ('self_connector', 'Self Connector'), ('bookwyrm_connector', 'Bookwyrm Connector')], max_length=255),
+            model_name="connector",
+            name="connector_file",
+            field=models.CharField(
+                choices=[
+                    ("openlibrary", "Openlibrary"),
+                    ("inventaire", "Inventaire"),
+                    ("self_connector", "Self Connector"),
+                    ("bookwyrm_connector", "Bookwyrm Connector"),
+                ],
+                max_length=255,
+            ),
         ),
     ]
