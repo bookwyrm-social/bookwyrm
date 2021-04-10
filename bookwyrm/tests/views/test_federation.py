c4bca42f642197602fef85f191982bc1c659998d
Mouse Reeve <mousereeve@riseup.net>
2021-04-10 09:52:11 -0700

    Adds test for edit view GET request

--- bookwyrm/tests/views/test_federation.py
+++ bookwyrm/tests/views/test_federation.py
@@ -62,3 +62,23 @@ class FederationViews(TestCase):
         view(request, server.id)
         server.refresh_from_db()
         self.assertEqual(server.status, "federated")
+
+    def test_edit_view_get(self):
+        """ there are so many views, this just makes sure it LOADS """
+        # create mode
+        view = views.EditFederatedServer.as_view()
+        request = self.factory.get("")
+        request.user = self.local_user
+        request.user.is_superuser = True
+
+        result = view(request, server=None)
+        self.assertIsInstance(result, TemplateResponse)
+        result.render()
+        self.assertEqual(result.status_code, 200)
+
+        # edit mode
+        server = models.FederatedServer.objects.create(server_name="hi.there.com")
+        result = view(request, server=server.id)
+        self.assertIsInstance(result, TemplateResponse)
+        result.render()
+        self.assertEqual(result.status_code, 200)
