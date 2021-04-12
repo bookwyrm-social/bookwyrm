92e9ff6cff26ac76b20932aa4f45f203b7127406
n2no1 <7995366-n2no1@users.noreply.gitlab.com>
2021-04-12 11:56:26 -0400

    update links in README and {bookwyrm,celerywyrm}/settings.py files
    the link to celery's homepage has been dead for at least several months
    activitypub.rocks now supports tls (yay!)
    the links to django's docs were for older versions of django than what bookwyrm uses

--- bookwyrm/settings.py
+++ bookwyrm/settings.py
@@ -166,7 +166,7 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = "/static/"
