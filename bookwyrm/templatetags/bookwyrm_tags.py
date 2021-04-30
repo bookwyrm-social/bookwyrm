b8fc4f99a3cdf911abe67731aac1defb1bfc9cdf
Mouse Reeve <mousereeve@riseup.net>
2021-04-30 11:11:18 -0700

    Don't calculate mutuals for logged out users

--- bookwyrm/templatetags/bookwyrm_tags.py
+++ bookwyrm/templatetags/bookwyrm_tags.py
@@ -241,4 +241,6 @@ def get_lang():
 def mutuals_count(context, user):
     """how many users that you follow, follow them"""
     viewer = context["request"].user
+    if not viewer.is_authenticated:
+        return None
     return user.followers.filter(id__in=viewer.following.all()).count()
