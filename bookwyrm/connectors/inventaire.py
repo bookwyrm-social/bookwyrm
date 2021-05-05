558c9c4d675ff2675c2b88171294d52b989105d4
Mouse Reeve <mousereeve@riseup.net>
2021-05-05 07:52:57 -0700

    Sort edition list for better import results from inventaire

--- bookwyrm/connectors/inventaire.py
+++ bookwyrm/connectors/inventaire.py
@@ -123,7 +123,7 @@ class Connector(AbstractConnector):
 
     def load_edition_data(self, work_uri):
         """get a list of editions for a work"""
-        url = "{:s}?action=reverse-claims&property=wdt:P629&value={:s}".format(
+        url = "{:s}?action=reverse-claims&property=wdt:P629&value={:s}&sort=true".format(
             self.books_url, work_uri
         )
         return get_data(url)
