36439506b646c245f5b68135021c922f501d48ed
Allie Signet <allie@cat-girl.gay>
2021-05-10 14:00:51 -0300

    add storygraph import

--- bookwyrm/importers/__init__.py
+++ bookwyrm/importers/__init__.py
@@ -3,3 +3,4 @@
 from .importer import Importer
 from .goodreads_import import GoodreadsImporter
 from .librarything_import import LibrarythingImporter
+from .storygraph_import import StorygraphImporter
