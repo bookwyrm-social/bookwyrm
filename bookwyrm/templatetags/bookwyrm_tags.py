ac2b63d0cd35ae3faedf764b1fa4d074b7ce192f
Mouse Reeve <mousereeve@riseup.net>
2021-05-11 15:14:42 -0700

    More templatetag files

--- bookwyrm/templatetags/bookwyrm_tags.py
+++ bookwyrm/templatetags/bookwyrm_tags.py
@@ -36,55 +36,10 @@ def get_user_rating(book, user):
     return 0
 
 
-@register.filter(name="replies")
-def get_replies(status):
-    """get all direct replies to a status"""
-    # TODO: this limit could cause problems
-    return models.Status.objects.filter(
-        reply_parent=status,
-        deleted=False,
-    ).select_subclasses()[:10]
-
-
-@register.filter(name="parent")
-def get_parent(status):
-    """get the reply parent for a status"""
-    return (
-        models.Status.objects.filter(id=status.reply_parent_id)
-        .select_subclasses()
-        .get()
-    )
-
-
-@register.filter(name="boosted_status")
-def get_boosted(boost):
-    """load a boosted status. have to do this or it won't get foreign keys"""
-    return (
-        models.Status.objects.select_subclasses().get(id=boost.boosted_status.id)
-    )
-
-
 @register.filter(name="book_description")
 def get_book_description(book):
     """use the work's text if the book doesn't have it"""
     return book.description or book.parent_work.description
-
-
-@register.filter(name="to_markdown")
-def get_markdown(content):
-    """convert markdown to html"""
-    if content:
-        return to_markdown(content)
-    return None
-
-
-@register.filter(name="mentions")
-def get_mentions(status, user):
-    """people to @ in a reply: the parent and all mentions"""
-    mentions = set([status.user] + list(status.mention_users.all()))
-    return (
-        " ".join("@" + get_user_identifier(m) for m in mentions if not m == user) + " "
-    )
 
 
 @register.filter(name="next_shelf")
---
+++ bookwyrm/templatetags/status_display.py
@@ -0,0 +1,46 @@
+""" template filters """
+from django import template
+
+from bookwyrm import models
+from bookwyrm.templatetags.utilities import get_user_identifier
+
+
+register = template.Library()
+
+
+@register.filter(name="mentions")
+def get_mentions(status, user):
+    """people to @ in a reply: the parent and all mentions"""
+    mentions = set([status.user] + list(status.mention_users.all()))
+    return (
+        " ".join("@" + get_user_identifier(m) for m in mentions if not m == user) + " "
+    )
+
+
+
+@register.filter(name="replies")
+def get_replies(status):
+    """get all direct replies to a status"""
+    # TODO: this limit could cause problems
+    return models.Status.objects.filter(
+        reply_parent=status,
+        deleted=False,
+    ).select_subclasses()[:10]
+
+
+@register.filter(name="parent")
+def get_parent(status):
+    """get the reply parent for a status"""
+    return (
+        models.Status.objects.filter(id=status.reply_parent_id)
+        .select_subclasses()
+        .get()
+    )
+
+
+@register.filter(name="boosted_status")
+def get_boosted(boost):
+    """load a boosted status. have to do this or it won't get foreign keys"""
+    return (
+        models.Status.objects.select_subclasses().get(id=boost.boosted_status.id)
+    )
