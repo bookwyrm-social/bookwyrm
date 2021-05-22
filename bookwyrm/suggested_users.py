5174260351c659530b0616e82e95f74eb94f7570
Mouse Reeve <mousereeve@riseup.net>
2021-05-22 15:57:08 -0700

    Python formatting

--- bookwyrm/suggested_users.py
+++ bookwyrm/suggested_users.py
@@ -47,9 +47,7 @@ class SuggestedUsers(RedisStore):
 
     def get_users_for_object(self, obj):  # pylint: disable=no-self-use
         """given a user, who might want to follow them"""
-        return models.User.objects.filter(
-            local=True,
-        ).exclude(
+        return models.User.objects.filter(local=True,).exclude(
             Q(id=obj.id) | Q(followers=obj) | Q(id__in=obj.blocks.all()) | Q(blocks=obj)
         )
 
