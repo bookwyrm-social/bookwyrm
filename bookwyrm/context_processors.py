f848dcd756972f41589687a0d9ce81735a7439c9
Joachim <joachim.robert@protonmail.com>
2021-05-26 18:20:22 +0200

    Fix issue with protocol

--- bookwyrm/context_processors.py
+++ bookwyrm/context_processors.py
@@ -1,16 +1,18 @@
 """ customize the info available in context for rendering templates """
 from bookwyrm import models
-from bookwyrm.settings import SITE_PATH, STATIC_URL, STATIC_PATH, MEDIA_URL, MEDIA_PATH
+from bookwyrm.settings import STATIC_URL, STATIC_PATH, MEDIA_URL, MEDIA_PATH
 
 
 def site_settings(request):  # pylint: disable=unused-argument
     """include the custom info about the site"""
+    request_protocol = "https://" if request.is_secure() else "http://"
+
     return {
         "site": models.SiteSettings.objects.get(),
         "active_announcements": models.Announcement.active_announcements(),
-        "site_path": SITE_PATH,
         "static_url": STATIC_URL,
         "media_url": MEDIA_URL,
         "static_path": STATIC_PATH,
         "media_path": MEDIA_PATH,
+        "request_protocol": request_protocol,
     }
