17f9682aabed377a2906b29d4045577844506aac
bcj <brendan@bcjbcj.ca>
2021-06-03 08:46:32 -0500

    Testfixes: Remove extra space + hopefully fix env

--- celerywyrm/settings.py
+++ celerywyrm/settings.py
@@ -1,7 +1,7 @@
 """ bookwyrm settings and configuration """
 from bookwyrm.settings import *
 
-CELERY_BROKER_URL =  CELERY_BROKER
+CELERY_BROKER_URL = CELERY_BROKER
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
 FLOWER_PORT = env("FLOWER_PORT")
