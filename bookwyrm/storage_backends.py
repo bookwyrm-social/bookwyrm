582c2d4f25a359932aaf5006dcceb9f63d040e0a
Joachim <joachim.robert@protonmail.com>
2021-06-19 12:04:30 +0200

    Fix Python formating

--- bookwyrm/storage_backends.py
+++ bookwyrm/storage_backends.py
@@ -4,12 +4,14 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class StaticStorage(S3Boto3Storage):  # pylint: disable=abstract-method
     """Storage class for Static contents"""
+
     location = "static"
     default_acl = "public-read"
 
 
 class ImagesStorage(S3Boto3Storage):  # pylint: disable=abstract-method
     """Storage class for Image files"""
+
     location = "images"
     default_acl = "public-read"
     file_overwrite = False
