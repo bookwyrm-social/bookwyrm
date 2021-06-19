8af13e5f7dabbc556e7d188fff54736f773f455a
Joachim <joachim.robert@protonmail.com>
2021-06-19 12:01:41 +0200

    Fix for pylint

--- bookwyrm/storage_backends.py
+++ bookwyrm/storage_backends.py
@@ -1,12 +1,15 @@
+"""Handles backends for storages"""
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
-class StaticStorage(S3Boto3Storage):
+class StaticStorage(S3Boto3Storage):  # pylint: disable=abstract-method
+    """Storage class for Static contents"""
     location = "static"
     default_acl = "public-read"
 
 
-class ImagesStorage(S3Boto3Storage):
+class ImagesStorage(S3Boto3Storage):  # pylint: disable=abstract-method
+    """Storage class for Image files"""
     location = "images"
     default_acl = "public-read"
     file_overwrite = False
