93a49c17cc249644d92c8d029e4b026ce67adb46
Mouse Reeve <mousereeve@riseup.net>
2021-06-27 11:25:59 -0700

    Updates rss feed tests

--- bookwyrm/tests/views/test_rss_feed.py
+++ bookwyrm/tests/views/test_rss_feed.py
@@ -1,5 +1,4 @@
 """ testing import """
-
 from unittest.mock import patch
 from django.test import RequestFactory, TestCase
 
@@ -7,62 +6,55 @@ from bookwyrm import models
 from bookwyrm.views import rss_feed
 
 
+@patch("bookwyrm.models.activitypub_mixin.broadcast_task.delay")
+@patch("bookwyrm.preview_images.generate_site_preview_image_task.delay")
+@patch("bookwyrm.preview_images.generate_user_preview_image_task.delay")
+@patch("bookwyrm.activitystreams.ActivityStream.get_activity_stream")
+@patch("bookwyrm.activitystreams.ActivityStream.add_status")
 class RssFeedView(TestCase):
     """rss feed behaves as expected"""
 
-    def setUp(self):
-        """test data"""
-        with patch("bookwyrm.preview_images.generate_site_preview_image_task.delay"):
-            self.site = models.SiteSettings.objects.create()
-
-        with patch("bookwyrm.preview_images.generate_user_preview_image_task.delay"):
-            self.user = models.User.objects.create_user(
-                "rss_user", "rss@test.rss", "password", local=True
-            )
-
-        with patch("bookwyrm.preview_images.generate_edition_preview_image_task.delay"):
-            work = models.Work.objects.create(title="Test Work")
-            self.book = models.Edition.objects.create(
-                title="Example Edition",
-                remote_id="https://example.com/book/1",
-                parent_work=work,
-            )
-
-            with patch("bookwyrm.models.activitypub_mixin.broadcast_task.delay"):
-                with patch("bookwyrm.activitystreams.ActivityStream.add_status"):
-                    with patch(
-                        "bookwyrm.preview_images.generate_edition_preview_image_task.delay"
-                    ):
-                        self.review = models.Review.objects.create(
-                            name="Review name",
-                            content="test content",
-                            rating=3,
-                            user=self.user,
-                            book=self.book,
-                        )
-
-                    self.quote = models.Quotation.objects.create(
-                        quote="a sickening sense",
-                        content="test content",
-                        user=self.user,
-                        book=self.book,
-                    )
-
-                    self.generatednote = models.GeneratedNote.objects.create(
-                        content="test content", user=self.user
-                    )
-
-        self.factory = RequestFactory()
-
-    @patch("bookwyrm.activitystreams.ActivityStream.get_activity_stream")
-    def test_rss_feed(self, _):
+    def test_rss_feed(self, *_):
         """load an rss feed"""
+        models.SiteSettings.objects.create()
+
+        user = models.User.objects.create_user(
+            "rss_user", "rss@test.rss", "password", local=True
+        )
+
+        work = models.Work.objects.create(title="Test Work")
+        book = models.Edition.objects.create(
+            title="Example Edition",
+            remote_id="https://example.com/book/1",
+            parent_work=work,
+        )
+
+        models.Review.objects.create(
+            name="Review name",
+            content="test content",
+            rating=3,
+            user=user,
+            book=book,
+        )
+
+        models.Quotation.objects.create(
+            quote="a sickening sense",
+            content="test content",
+            user=user,
+            book=book,
+        )
+
+        models.GeneratedNote.objects.create(
+            content="test content", user=user, note_type="READ"
+        )
+
+        factory = RequestFactory()
+
         view = rss_feed.RssFeed()
-        request = self.factory.get("/user/rss_user/rss")
-        request.user = self.user
-        with patch("bookwyrm.models.SiteSettings.objects.get") as site:
-            site.return_value = self.site
-            result = view(request, username=self.user.username)
+        request = factory.get("/user/rss_user/rss")
+        request.user = user
+
+        result = view(request, username=user.username)
         self.assertEqual(result.status_code, 200)
 
         self.assertIn(b"Status updates from rss_user", result.content)
