8d476804c2041e526524265309eefa8390a87aeb
Mouse Reeve <mousereeve@riseup.net>
2021-08-01 07:38:16 -0700

    Potential speedups

---
+++ bookwyrm/test_settings.py
@@ -0,0 +1,14 @@
+""" bookwyrm settings and configuration """
+from bookwyrm.settings import *
+
+MIGRATION_MODULES = {
+    'auth': None,
+    'contenttypes': None,
+    'default': None,
+    'sessions': None,
+
+    'core': None,
+    'profiles': None,
+    'snippets': None,
+    'scaffold_templates': None,
+}
