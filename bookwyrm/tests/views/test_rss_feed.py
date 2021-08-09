fd56379518f93aa56dcb2e1a64f51fc2a400cc47
Mouse Reeve <mousereeve@riseup.net>
2021-08-09 07:38:43 -0700

    Updates rss feed view

--- bookwyrm/tests/views/test_rss_feed.py
+++ bookwyrm/tests/views/test_rss_feed.py
@@ -1,5 +1,4 @@
 """ testing import """
-
 from unittest.mock import patch
 from django.test import RequestFactory, TestCase
 
@@ -7,57 +6,51 @@ from bookwyrm import models
 from bookwyrm.views import rss_feed
 
 
+@patch("bookwyrm.models.activitypub_mixin.broadcast_task.delay")
+@patch("bookwyrm.activitystreams.ActivityStream.get_activity_stream")
+@patch("bookwyrm.activitystreams.ActivityStream.add_status")
 class RssFeedView(TestCase):
     """rss feed behaves as expected"""
 
-    def setUp(self):
-        """test data"""
-        self.site = models.SiteSettings.objects.create()
+    def test_rss_feed(self, *_):
+        """load an rss feed"""
+        models.SiteSettings.objects.create()
 
-        with patch("bookwyrm.suggested_users.rerank_suggestions_task.delay"):
-            self.user = models.User.objects.create_user(
-                "rss_user", "rss@test.rss", "password", local=True
-            )
+        user = models.User.objects.create_user(
+            "rss_user", "rss@test.rss", "password", local=True
+        )
 
         work = models.Work.objects.create(title="Test Work")
-        self.book = models.Edition.objects.create(
+        book = models.Edition.objects.create(
             title="Example Edition",
             remote_id="https://example.com/book/1",
             parent_work=work,
         )
 
-        with patch("bookwyrm.models.activitypub_mixin.broadcast_task.delay"):
-            with patch("bookwyrm.activitystreams.ActivityStream.add_status"):
-                self.review = models.Review.objects.create(
-                    name="Review name",
-                    content="test content",
-                    rating=3,
-                    user=self.user,
-                    book=self.book,
-                )
+        models.Review.objects.create(
+            name="Review name",
+            content="test content",
+            rating=3,
+            user=user,
+            book=book,
+        )
 
-                self.quote = models.Quotation.objects.create(
-                    quote="a sickening sense",
-                    content="test content",
-                    user=self.user,
-                    book=self.book,
-                )
+        models.Quotation.objects.create(
+            quote="a sickening sense",
+            content="test content",
+            user=user,
+            book=book,
+        )
 
-                self.generatednote = models.GeneratedNote.objects.create(
-                    content="test content", user=self.user
-                )
+        models.ReadStatus.objects.create(content="test content", user=user)
 
-        self.factory = RequestFactory()
+        factory = RequestFactory()
 
-    @patch("bookwyrm.activitystreams.ActivityStream.get_activity_stream")
-    def test_rss_feed(self, _):
-        """load an rss feed"""
         view = rss_feed.RssFeed()
-        request = self.factory.get("/user/rss_user/rss")
-        request.user = self.user
-        with patch("bookwyrm.models.SiteSettings.objects.get") as site:
-            site.return_value = self.site
-            result = view(request, username=self.user.username)
+        request = factory.get("/user/rss_user/rss")
+        request.user = user
+
+        result = view(request, username=user.username)
         self.assertEqual(result.status_code, 200)
 
         self.assertIn(b"Status updates from rss_user", result.content)
