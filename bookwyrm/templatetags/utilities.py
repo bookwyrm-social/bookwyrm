bf6ee9f53c391b75b729bd4678c6c5843887385a
Joachim <joachim.robert@protonmail.com>
2021-08-20 18:06:09 +0200

    Thumbnails: fix default size definition in template utility

--- bookwyrm/templatetags/utilities.py
+++ bookwyrm/templatetags/utilities.py
@@ -56,6 +56,8 @@ def truncatepath(value, arg):
 @register.simple_tag(takes_context=False)
 def get_book_cover_thumbnail(book, size="medium", ext="jpg"):
     """Returns a book thumbnail at the specified size and extension, with fallback if needed"""
+    if size == "":
+        size = "medium"
     try:
         cover_thumbnail = getattr(book, "cover_bw_book_%s_%s" % (size, ext))
         return cover_thumbnail.url
