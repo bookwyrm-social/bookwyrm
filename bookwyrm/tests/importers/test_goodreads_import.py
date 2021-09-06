38dc640dd50a0c2c5efdcb0e68b1110704c2741f
Mouse Reeve <mousereeve@riseup.net>
2021-09-06 15:10:52 -0700

    Mock for goodreads import test`

--- bookwyrm/tests/importers/test_goodreads_import.py
+++ bookwyrm/tests/importers/test_goodreads_import.py
@@ -23,6 +23,7 @@ def make_date(*args):
 # pylint: disable=consider-using-with
 @patch("bookwyrm.suggested_users.rerank_suggestions_task.delay")
 @patch("bookwyrm.activitystreams.populate_stream_task.delay")
+@patch("bookwyrm.activitystreams.add_book_statuses_task.delay.delay")
 class GoodreadsImport(TestCase):
     """importing from goodreads csv"""
 
