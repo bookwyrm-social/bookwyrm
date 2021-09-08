03b608565dcc12c199ce8f2c009cf4743475f207
Mouse Reeve <mousereeve@riseup.net>
2021-09-08 13:58:10 -0700

    Save status drafts in localstorage

---
+++ bookwyrm/static/js/status_cache.js
@@ -0,0 +1,40 @@
+/* exported StatusCache */
+/* globals BookWyrm */
+
+let StatusCache = new class {
+    constructor() {
+        document.querySelectorAll('[data-cache-draft]')
+            .forEach(t => t.addEventListener('change', this.updateDraft.bind(this)));
+        document.querySelectorAll('[data-cache-draft]')
+            .forEach(t => this.populateDraft(t));
+    }
+
+    /**
+     * Update localStorage copy of drafted status
+     *
+     * @param  {Event} event
+     * @return {undefined}
+     */
+    updateDraft(event) {
+        // Used in set reading goal
+        let key = event.target.dataset.cacheDraft;
+        let value = event.target.value;
+
+        window.localStorage.setItem(key, value);
+    }
+
+    /**
+     * Toggle display of a DOM node based on its value in the localStorage.
+     *
+     * @param {object} node - DOM node to toggle.
+     * @return {undefined}
+     */
+    populateDraft(node) {
+        // Used in set reading goal
+        let key = node.dataset.cacheDraft;
+        let value = window.localStorage.getItem(key);
+
+        node.value = value;
+    }
+}();
+
