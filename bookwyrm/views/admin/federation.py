03a5ac9c3fead8688c43cd8d7c16d9870b9f85a4
Mouse Reeve <mousereeve@riseup.net>
2021-09-11 14:27:42 -0700

    Use elided page range for federated peer list

--- bookwyrm/views/admin/federation.py
+++ bookwyrm/views/admin/federation.py
@@ -33,9 +33,13 @@ class Federation(View):
         servers = servers.order_by(sort)
 
         paginated = Paginator(servers, PAGE_LENGTH)
+        page = paginated.get_page(request.GET.get("page"))
 
         data = {
-            "servers": paginated.get_page(request.GET.get("page")),
+            "servers": page,
+            "page_range": paginated.get_elided_page_range(
+                page.number, on_each_side=2, on_ends=1
+            ),
             "sort": sort,
             "form": forms.ServerForm(),
         }
