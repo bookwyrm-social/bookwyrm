b921d666cffe8d8c993390525a73158a94444165
Hugh Rundle <hugh@hughrundle.net>
2021-09-26 15:55:16 +1000

    add group field to ListForm

--- bookwyrm/forms.py
+++ bookwyrm/forms.py
@@ -293,7 +293,7 @@ class AnnouncementForm(CustomForm):
 class ListForm(CustomForm):
     class Meta:
         model = models.List
-        fields = ["user", "name", "description", "curation", "privacy"]
+        fields = ["user", "name", "description", "curation", "privacy", "group"]
 
 class GroupForm(CustomForm):
     class Meta:
