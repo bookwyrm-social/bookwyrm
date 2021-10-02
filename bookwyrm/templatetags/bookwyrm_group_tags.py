0f3be40957e65e956b16dedb5c30c13942badb78
Hugh Rundle <hugh@hughrundle.net>
2021-10-02 10:47:42 +1000

    fix group references in templates
    Let's do this the sensible way huh, by using backwards references to memberships etc
    
    Also adds filters for is_member and is_invited so we don't have to do weird things in group Views

---
+++ bookwyrm/templatetags/bookwyrm_group_tags.py
@@ -0,0 +1,19 @@
+""" template filters """
+from django import template
+
+from bookwyrm import models
+
+register = template.Library()
+
+
+@register.filter(name="is_member")
+def is_member(group, user):
+    """whether or not the user is a member of this group"""
+
+    return models.BookwyrmGroupMember.objects.filter(group=group,user=user).exists()
+
+@register.filter(name="is_invited")
+def is_invited(group, user):
+    """whether or not the user has a pending invitation to join this group"""
+
+    return models.GroupMemberInvitation.objects.filter(group=group,user=user).exists()
