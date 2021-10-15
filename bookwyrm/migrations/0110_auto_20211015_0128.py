60b910172de788775b44aa9f7d58ad674632c94f
Mouse Reeve <mousereeve@riseup.net>
2021-10-15 10:09:34 -0700

    Update migration

--- bookwyrm/migrations/0110_auto_20211015_0128.py
+++ bookwyrm/migrations/0110_auto_20211015_0128.py
@@ -7,7 +7,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("bookwyrm", "0109_status_edited"),
+        ("bookwyrm", "0109_status_edited_date"),
     ]
 
     operations = [
