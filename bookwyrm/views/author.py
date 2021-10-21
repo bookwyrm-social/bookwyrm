de93beca84e5f2dcee9922486d99b6cc662a4525
Mouse Reeve <mousereeve@riseup.net>
2021-10-20 17:51:42 -0700

    Adds shelve buttons to books on author page

--- bookwyrm/views/author.py
+++ bookwyrm/views/author.py
@@ -29,7 +29,7 @@ class Author(View):
         ).order_by("-edition_rank")
 
         books = (
-            models.Edition.objects.filter(
+            models.Edition.viewer_aware_objects(request.user).filter(
                 Q(authors=author) | Q(parent_work__authors=author)
             )
             .annotate(default_id=Subquery(default_editions.values("id")[:1]))
