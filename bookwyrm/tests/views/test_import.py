ffcaef055959b64576b9b01bbc0f84715cafb248
Mouse Reeve <mousereeve@riseup.net>
2021-11-11 13:39:58 -0800

    Python formatting

--- bookwyrm/tests/views/test_import.py
+++ bookwyrm/tests/views/test_import.py
@@ -59,11 +59,11 @@ class ImportViews(TestCase):
         form.data["privacy"] = "public"
         form.data["include_reviews"] = False
         csv_file = pathlib.Path(__file__).parent.joinpath("../data/goodreads.csv")
-        form.data[
-            "csv_file"
-        ] = SimpleUploadedFile(
+        form.data["csv_file"] = SimpleUploadedFile(
             # pylint: disable=consider-using-with
-            csv_file, open(csv_file, "rb").read(), content_type="text/csv"
+            csv_file,
+            open(csv_file, "rb").read(),
+            content_type="text/csv",
         )
 
         request = self.factory.post("", form.data)
