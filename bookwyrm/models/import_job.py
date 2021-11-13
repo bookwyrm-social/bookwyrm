1e46de4c9d4a184dedf127a4245fb75254a21b3f
Mouse Reeve <mousereeve@riseup.net>
2021-11-13 11:44:05 -0800

    Associate imported review with import item

--- bookwyrm/models/import_job.py
+++ bookwyrm/models/import_job.py
@@ -60,6 +60,9 @@ class ImportItem(models.Model):
         related_name="book_guess",
     )
     fail_reason = models.TextField(null=True)
+    linked_review = models.ForeignKey(
+        "Review", on_delete=models.SET_NULL, null=True, blank=True
+    )
 
     def resolve(self):
         """try various ways to lookup a book"""
