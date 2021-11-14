8495cf8a45e90b463e0e70b10956957f90ff4365
Hugh Rundle <hugh@hughrundle.net>
2021-11-14 21:21:37 +1100

    don't delete non-form data when editing authors
    fixes #1584
    
    This is a temporary fix. As Mouse has suggested, ultimately it would be good to re-import data from one or more of the linked data sources if there is anything missing.

--- bookwyrm/forms.py
+++ bookwyrm/forms.py
@@ -201,12 +201,17 @@ class EditionForm(CustomForm):
 class AuthorForm(CustomForm):
     class Meta:
         model = models.Author
-        exclude = [
-            "remote_id",
-            "origin_id",
-            "created_date",
-            "updated_date",
-            "search_vector",
+        fields = [
+            "last_edited_by",
+            "name",
+            "aliases",
+            "bio",
+            "wikipedia_link",
+            "born",
+            "died",
+            "openlibrary_key",
+            "librarything_key",
+            "goodreads_key",
         ]
 
 
