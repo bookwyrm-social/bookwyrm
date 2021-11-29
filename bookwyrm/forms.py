28f0882ba61dec1f41748d7e0ea9477ae1f71168
Joachim <joachim.robert@protonmail.com>
2021-11-29 22:39:01 +0100

    Handle Author form errors

--- bookwyrm/forms.py
+++ bookwyrm/forms.py
@@ -236,7 +236,39 @@ class AuthorForm(CustomForm):
             "librarything_key",
             "goodreads_key",
         ]
-
+        widgets = {
+            "name": forms.TextInput(
+                attrs={"aria-describedby": "desc_name"}
+            ),
+            "aliases": forms.TextInput(
+                attrs={"aria-describedby": "desc_aliases"}
+            ),
+            "bio": forms.Textarea(
+                attrs={"aria-describedby": "desc_bio"}
+            ),
+            "wikipedia_link": forms.TextInput(
+                attrs={"aria-describedby": "desc_wikipedia_link"}
+            ),
+            "born": forms.SelectDateWidget(
+                attrs={"aria-describedby": "desc_born"}
+            ),
+            "died": forms.SelectDateWidget(
+                attrs={"aria-describedby": "desc_died"}
+            ),
+            "oepnlibrary_key": forms.TextInput(
+                attrs={"aria-describedby": "desc_oepnlibrary_key"}
+            ),
+            "inventaire_id": forms.TextInput(
+                attrs={"aria-describedby": "desc_inventaire_id"}
+            ),
+            "librarything_key": forms.TextInput(
+                attrs={"aria-describedby": "desc_librarything_key"}
+            ),
+            "goodreads_key": forms.TextInput(
+                attrs={"aria-describedby": "desc_goodreads_key"}
+            ),
+        }
+        
 
 class ImportForm(forms.Form):
     csv_file = forms.FileField()
