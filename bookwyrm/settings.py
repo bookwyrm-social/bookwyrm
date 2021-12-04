e19fe3321b1b72b2b3af35a519802b0f8072b646
Mouse Reeve <mousereeve@riseup.net>
2021-12-04 14:47:06 -0800

    Javascript cache buster

--- bookwyrm/settings.py
+++ bookwyrm/settings.py
@@ -14,7 +14,7 @@ VERSION = "0.1.0"
 PAGE_LENGTH = env("PAGE_LENGTH", 15)
 DEFAULT_LANGUAGE = env("DEFAULT_LANGUAGE", "English")
 
-JS_CACHE = "3eb4edb1"
+JS_CACHE = "3891b373"
 
 # email
 EMAIL_BACKEND = env("EMAIL_BACKEND", "django.core.mail.backends.smtp.EmailBackend")
