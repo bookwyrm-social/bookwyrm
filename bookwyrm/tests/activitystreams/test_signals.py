0c193b6ce18c3793947cbf4dafdeee7976913d83
Mouse Reeve <mousereeve@riseup.net>
2021-12-09 20:33:11 -0800

    ActivityStream mocks

--- bookwyrm/tests/activitystreams/test_signals.py
+++ bookwyrm/tests/activitystreams/test_signals.py
@@ -12,7 +12,7 @@ class ActivitystreamsSignals(TestCase):
         """use a test csv"""
         with patch("bookwyrm.suggested_users.rerank_suggestions_task.delay"), patch(
             "bookwyrm.activitystreams.populate_stream_task.delay"
-        ):
+        ), patch("bookwyrm.lists_stream.populate_lists_task.delay"):
             self.local_user = models.User.objects.create_user(
                 "mouse", "mouse@mouse.mouse", "password", local=True, localname="mouse"
             )
