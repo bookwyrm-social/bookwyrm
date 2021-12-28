86e8d36b2599d5e3de0999d73abd5a28f8d9322f
Mouse Reeve <mousereeve@riseup.net>
2021-12-28 07:02:52 -0800

    Adds annual review translations for Galician and Spanish

--- bookwyrm/settings.py
+++ bookwyrm/settings.py
@@ -14,7 +14,7 @@ VERSION = "0.1.0"
 PAGE_LENGTH = env("PAGE_LENGTH", 15)
 DEFAULT_LANGUAGE = env("DEFAULT_LANGUAGE", "English")
 
-JS_CACHE = "3891b373"
+JS_CACHE = "2d3181e1"
 
 # email
 EMAIL_BACKEND = env("EMAIL_BACKEND", "django.core.mail.backends.smtp.EmailBackend")
