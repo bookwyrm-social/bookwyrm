879a410808e450838c70e781925c23f1885ee2db
Joel Bradshaw <cincodenada@gmail.com>
2021-12-28 13:45:39 -0800

    Attempt to use env variable for backup script
    This should be available via docker, and we shouldn't have the database
    name hardcoded anywhere

--- postgres-docker/backup.sh
+++ postgres-docker/backup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+if [ -z "$POSTGRES_DB" ]; then
+    echo "Database not specified, defaulting to bookwyrm"
+fi
+BACKUP_DB=${POSTGRES_DB:-bookwyrm}
 filename=backup__$(date +%F)
-pg_dump -U bookwyrm  > /backups/$filename.sql
+pg_dump -U $BACKUP_DB > /backups/$filename.sql
