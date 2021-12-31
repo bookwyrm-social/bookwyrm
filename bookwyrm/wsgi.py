8b4f93dd4e9b1c1bd06fd365d74acf1210372443
Joel Bradshaw <cincodenada@gmail.com>
2021-12-31 03:46:21 -0800

    Integrate open telemetry
    This allows us to export to anyone that takes OTLP, which is most of the
    major players, I think! Nifty!
    
    Kinda like the S3 config but for tracing, you can slot in any provider
    that supports it via environment variables
    
    This uses the Django instrumentation, which gets us a bunch of nifty
    stuff right out of the box.

--- bookwyrm/wsgi.py
+++ bookwyrm/wsgi.py
@@ -10,9 +10,19 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 import os
 from environs import Env
 from django.core.wsgi import get_wsgi_application
+from opentelemetry.instrumentation.django import DjangoInstrumentor
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
 Env.read_env()
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bookwyrm.settings")
+DjangoInstrumentor().instrument()
+trace.set_tracer_provider(TracerProvider())
+trace.get_tracer_provider().add_span_processor(
+    BatchSpanProcessor(OTLPSpanExporter())
+)
 
 application = get_wsgi_application()
