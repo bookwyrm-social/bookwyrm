5b3ff0cf82d25fe6d5bca03ebe7196e312b4c071
Joel Bradshaw <cincodenada@gmail.com>
2022-01-01 14:11:45 -0800

    Instrument celery, move init into apps.py

--- bookwyrm/wsgi.py
+++ bookwyrm/wsgi.py
@@ -10,13 +10,9 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 import os
 from environs import Env
 from django.core.wsgi import get_wsgi_application
-from bookwyrm.telemetry import InstallOpenTelemetry
 
 Env.read_env()
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bookwyrm.settings")
-
-if 'OTEL_EXPORTER_OTLP_ENDPOINT' in os.environ:
-    InstallOpenTelemetry()
 
 application = get_wsgi_application()
