f5f861ce25bef9ae322d3e8aead4b70bcc0d8032
Joel Bradshaw <cincodenada@gmail.com>
2022-01-01 14:49:28 -0800

    Make it black 🎸

--- bookwyrm/telemetry/open_telemetry.py
+++ bookwyrm/telemetry/open_telemetry.py
@@ -6,9 +6,12 @@ from opentelemetry.sdk.trace.export import BatchSpanProcessor
 trace.set_tracer_provider(TracerProvider())
 trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(OTLPSpanExporter()))
 
+
 def instrumentDjango():
     from opentelemetry.instrumentation.django import DjangoInstrumentor
+
     DjangoInstrumentor().instrument()
+
 
 def instrumentCelery():
     from opentelemetry.instrumentation.celery import CeleryInstrumentor
--- celerywyrm/apps.py
+++ celerywyrm/apps.py
@@ -1,10 +1,13 @@
 from django.apps import AppConfig
 from celerywyrm import settings
 
+
 class CelerywyrmConfig(AppConfig):
-    name = 'celerywyrm'
+    name = "celerywyrm"
     verbose_name = "BookWyrm Celery"
+
     def ready(self):
         if settings.OTEL_EXPORTER_OTLP_ENDPOINT:
             from bookwyrm.telemetry import open_telemetry
+
             open_telemetry.instrumentCelery()
