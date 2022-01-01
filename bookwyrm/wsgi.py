3d7f73d73c63750d92c2904da2f1c9493919049b
Joel Bradshaw <cincodenada@gmail.com>
2022-01-01 13:16:17 -0800

    Document OTLP in env, only load if env vars exist
    Also move telemetry into its own file, all those imports seemed like
    unnecessary clutter

--- bookwyrm/wsgi.py
+++ bookwyrm/wsgi.py
@@ -10,17 +10,13 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 import os
 from environs import Env
 from django.core.wsgi import get_wsgi_application
-from opentelemetry.instrumentation.django import DjangoInstrumentor
-from opentelemetry import trace
-from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from bookwyrm.telemetry import InstallOpenTelemetry
 
 Env.read_env()
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bookwyrm.settings")
-DjangoInstrumentor().instrument()
-trace.set_tracer_provider(TracerProvider())
-trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(OTLPSpanExporter()))
+
+if 'OTEL_EXPORTER_OTLP_ENDPOINT' in os.environ:
+    InstallOpenTelemetry()
 
 application = get_wsgi_application()
