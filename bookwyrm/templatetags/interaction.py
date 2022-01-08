2cca9fab2d880b6f16ae13d2923de89abe239270
Mouse Reeve <mousereeve@riseup.net>
2022-01-08 12:05:42 -0800

    Cache user relationship for follow buttons

--- bookwyrm/templatetags/interaction.py
+++ bookwyrm/templatetags/interaction.py
@@ -32,3 +32,27 @@ def get_user_boosted(user, status):
 def get_user_saved_lists(user, book_list):
     """did the user save a list"""
     return user.saved_lists.filter(id=book_list.id).exists()
+
+@register.simple_tag(takes_context=True)
+def get_relationship(context, user_object):
+    """caches the relationship between the logged in user and another user"""
+    user = context["request"].user
+    return cache.get(f"relationship-{user.id}-{user_object.id}") or cache.set(
+        get_relationship_name(user, user_object),
+        timeout=259200,
+    )
+
+def get_relationship_name(user, user_object):
+    """returns the relationship type"""
+    types = {
+        "is_following": False,
+        "is_follow_pending": False,
+        "is_blocked": False,
+    }
+    if user_object in user.blocks.all():
+        types["is_blocked"] = True
+    elif user_object in user.following.all():
+        types["is_following"] = True
+    elif user_object in user.follower_requests.all():
+        types["is_follow_pending"] = True
+    return types
