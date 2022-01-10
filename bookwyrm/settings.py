af3c84cd8741334e723d09a895bf6576143deac1
Joel Bradshaw <joel@cincodenada.com>
2022-01-10 06:43:43 +0000

    Add basic logging config

--- bookwyrm/settings.py
+++ bookwyrm/settings.py
@@ -106,6 +106,27 @@ TEMPLATES = [
     },
 ]
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+            'propagate': False,
+        },
+    },
+}
+
 
 WSGI_APPLICATION = "bookwyrm.wsgi.application"
 
