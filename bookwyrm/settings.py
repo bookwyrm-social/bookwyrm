8ba3a4ab00d40dcda805396f291e0ccfd14ce8df
Mouse Reeve <mousereeve@riseup.net>
2022-01-10 11:47:52 -0800

    Adds link confirmation modal

--- bookwyrm/settings.py
+++ bookwyrm/settings.py
@@ -14,7 +14,7 @@ VERSION = "0.2.0"
 PAGE_LENGTH = env("PAGE_LENGTH", 15)
 DEFAULT_LANGUAGE = env("DEFAULT_LANGUAGE", "English")
 
-JS_CACHE = "2d3181e1"
+JS_CACHE = "a47cc2ca"
 
 # email
 EMAIL_BACKEND = env("EMAIL_BACKEND", "django.core.mail.backends.smtp.EmailBackend")
