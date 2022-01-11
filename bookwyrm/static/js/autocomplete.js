60761b19ba94ae153c3e90fbd27760c9171bc692
Mouse Reeve <mousereeve@riseup.net>
2022-01-10 16:55:30 -0800

    Run prettier

--- bookwyrm/static/js/autocomplete.js
+++ bookwyrm/static/js/autocomplete.js
@@ -1,5 +1,5 @@
-(function() {
-    'use strict';
+(function () {
+    "use strict";
 
     /**
      * Suggest a completion as a user types
@@ -30,7 +30,7 @@
         suggestionsBox.innerHTML = "";
 
         // Populate suggestions box
-        suggestions.forEach(suggestion => {
+        suggestions.forEach((suggestion) => {
             const suggestionItem = document.createElement("option");
 
             suggestionItem.textContent = suggestion;
@@ -40,7 +40,7 @@
 
     function getSuggestions(input, trie) {
         // Follow the trie through the provided input
-        input.split("").forEach(letter => {
+        input.split("").forEach((letter) => {
             trie = trie[letter];
 
             if (!trie) {
@@ -58,55 +58,54 @@
     function searchTrie(trie) {
         const options = Object.values(trie);
 
-        if (typeof trie == 'string') {
+        if (typeof trie == "string") {
             return [trie];
         }
 
-        return options.map(option => {
-            const newTrie = option;
+        return options
+            .map((option) => {
+                const newTrie = option;
 
-            if (typeof newTrie == 'string') {
-                return [newTrie];
-            }
+                if (typeof newTrie == "string") {
+                    return [newTrie];
+                }
 
-            return searchTrie(newTrie);
-        }).reduce((prev, next) => prev.concat(next));
+                return searchTrie(newTrie);
+            })
+            .reduce((prev, next) => prev.concat(next));
     }
 
-    document
-        .querySelectorAll('[data-autocomplete]')
-        .forEach(input => {
-            input.addEventListener('input', autocomplete);
-        });
+    document.querySelectorAll("[data-autocomplete]").forEach((input) => {
+        input.addEventListener("input", autocomplete);
+    });
 })();
 
 const mimetypeTrie = {
-    "a": {
-        "a": {
-            "c": "AAC",
+    a: {
+        a: {
+            c: "AAC",
         },
-        "z": {
-            "w": "AZW",
-        }
+        z: {
+            w: "AZW",
+        },
     },
-    "d": "Daisy",
-    "e": "ePub",
-    "f": "FLAC",
-    "h": "HTML",
-    "m": {
-        "4": {
-            "a": "M4A",
-            "b": "M4B",
+    d: "Daisy",
+    e: "ePub",
+    f: "FLAC",
+    h: "HTML",
+    m: {
+        4: {
+            a: "M4A",
+            b: "M4B",
         },
-        "o": "MOBI",
-        "p": "MP3",
+        o: "MOBI",
+        p: "MP3",
     },
-    "o": "OGG",
-    "p": {
-        "d": {
-            "f": "PDF",
+    o: "OGG",
+    p: {
+        d: {
+            f: "PDF",
         },
-        "l": "Plaintext",
+        l: "Plaintext",
     },
 };
-
