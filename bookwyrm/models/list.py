d0a4c78d02d867fe267b2f723ddfa7637e8ed4d6
Mouse Reeve <mousereeve@riseup.net>
2022-01-24 12:01:17 -0800

    Limit length on list item note

--- bookwyrm/models/list.py
+++ bookwyrm/models/list.py
@@ -125,7 +125,7 @@ class ListItem(CollectionItemMixin, BookWyrmModel):
     user = fields.ForeignKey(
         "User", on_delete=models.PROTECT, activitypub_field="actor"
     )
-    notes = fields.TextField(blank=True, null=True)
+    notes = fields.TextField(blank=True, null=True, max_length=300)
     approved = models.BooleanField(default=True)
     order = fields.IntegerField()
     endorsement = models.ManyToManyField("User", related_name="endorsers")
