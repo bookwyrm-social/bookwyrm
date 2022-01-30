ae0e67f0760e74b9e146996f0803e4f7e235f4b0
Mouse Reeve <mousereeve@riseup.net>
2022-01-30 09:31:57 -0800

    Fixes form element display inside details panel

--- bookwyrm/templatetags/shelf_tags.py
+++ bookwyrm/templatetags/shelf_tags.py
@@ -36,19 +36,22 @@ def get_next_shelf(current_shelf):
 def active_shelf(context, book):
     """check what shelf a user has a book on, if any"""
     user = context["request"].user
-    return cache.get_or_set(
-        f"active_shelf-{user.id}-{book.id}",
-        lambda u, b: (
-            models.ShelfBook.objects.filter(
-                shelf__user=u,
-                book__parent_work__editions=b,
-            ).first()
-            or False
-        ),
-        user,
-        book,
-        timeout=15552000,
-    ) or {"book": book}
+    return (
+        cache.get_or_set(
+            f"active_shelf-{user.id}-{book.id}",
+            lambda u, b: (
+                models.ShelfBook.objects.filter(
+                    shelf__user=u,
+                    book__parent_work__editions=b,
+                ).first()
+                or False
+            ),
+            user,
+            book,
+            timeout=15552000,
+        )
+        or {"book": book}
+    )
 
 
 @register.simple_tag(takes_context=False)
