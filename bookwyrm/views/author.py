28a8edfdc460dba2692790195f1eef44498cfb8f
Mouse Reeve <mousereeve@riseup.net>
2022-02-04 19:06:47 -0800

    Fixes author page query

--- bookwyrm/views/author.py
+++ bookwyrm/views/author.py
@@ -1,6 +1,7 @@
 """ the good people stuff! the authors! """
 from django.contrib.auth.decorators import login_required, permission_required
 from django.core.paginator import Paginator
+from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect
 from django.template.response import TemplateResponse
 from django.utils.decorators import method_decorator
@@ -26,7 +27,7 @@ class Author(View):
             return ActivitypubResponse(author.to_activity())
 
         books = models.Work.objects.filter(
-            authors=author, editions__authors=author
+            Q(authors=author) | Q(editions__authors=author)
         ).distinct()
 
         paginated = Paginator(books, PAGE_LENGTH)
