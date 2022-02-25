65bd3945e70db3224ce953e4b0acea6db0f288e5
Mouse Reeve <mousereeve@riseup.net>
2022-02-25 12:52:35 -0800

    Prettify tabs file

--- bookwyrm/static/js/tabs.js
+++ bookwyrm/static/js/tabs.js
@@ -1,11 +1,11 @@
 /* exported TabGroup */
 
 /*
-* The content below is licensed according to the W3C Software License at
-* https://www.w3.org/Consortium/Legal/2015/copyright-software-and-document
-* Heavily modified to web component by Zach Leatherman
-* Modified back to vanilla JavaScript with support for Bulma markup and nested tabs by Ned Zimmerman
-*/
+ * The content below is licensed according to the W3C Software License at
+ * https://www.w3.org/Consortium/Legal/2015/copyright-software-and-document
+ * Heavily modified to web component by Zach Leatherman
+ * Modified back to vanilla JavaScript with support for Bulma markup and nested tabs by Ned Zimmerman
+ */
 class TabGroup {
     constructor(container) {
         this.container = container;
@@ -15,7 +15,7 @@ class TabGroup {
         this.panels = this.container.querySelectorAll(':scope > [role="tabpanel"]');
         this.delay = this.determineDelay();
 
-        if(!this.tablist || !this.tabs.length || !this.panels.length) {
+        if (!this.tablist || !this.tabs.length || !this.panels.length) {
             return;
         }
 
@@ -32,7 +32,7 @@ class TabGroup {
             left: 37,
             up: 38,
             right: 39,
-            down: 40
+            down: 40,
         };
     }
 
@@ -42,19 +42,19 @@ class TabGroup {
             37: -1,
             38: -1,
             39: 1,
-            40: 1
+            40: 1,
         };
     }
 
     initTabs() {
         let count = 0;
-        for(let tab of this.tabs) {
+        for (let tab of this.tabs) {
             let isSelected = tab.getAttribute("aria-selected") === "true";
             tab.setAttribute("tabindex", isSelected ? "0" : "-1");
 
-            tab.addEventListener('click', this.clickEventListener.bind(this));
-            tab.addEventListener('keydown', this.keydownEventListener.bind(this));
-            tab.addEventListener('keyup', this.keyupEventListener.bind(this));
+            tab.addEventListener("click", this.clickEventListener.bind(this));
+            tab.addEventListener("keydown", this.keydownEventListener.bind(this));
+            tab.addEventListener("keyup", this.keyupEventListener.bind(this));
 
             tab.index = count++;
         }
@@ -64,8 +64,8 @@ class TabGroup {
         let selectedPanelId = this.tablist
             .querySelector('[role="tab"][aria-selected="true"]')
             .getAttribute("aria-controls");
-        for(let panel of this.panels) {
-            if(panel.getAttribute("id") !== selectedPanelId) {
+        for (let panel of this.panels) {
+            if (panel.getAttribute("id") !== selectedPanelId) {
                 panel.setAttribute("hidden", "");
             }
             panel.setAttribute("tabindex", "0");
@@ -122,7 +122,7 @@ class TabGroup {
     // In all other cases only left and right arrow function.
     determineOrientation(event) {
         var key = event.keyCode;
-        var vertical = this.tablist.getAttribute('aria-orientation') == 'vertical';
+        var vertical = this.tablist.getAttribute("aria-orientation") == "vertical";
         var proceed = false;
 
         if (vertical) {
@@ -130,8 +130,7 @@ class TabGroup {
                 event.preventDefault();
                 proceed = true;
             }
-        }
-        else {
+        } else {
             if (key === this.keys.left || key === this.keys.right) {
                 proceed = true;
             }
@@ -148,7 +147,7 @@ class TabGroup {
         var pressed = event.keyCode;
 
         for (let tab of this.tabs) {
-            tab.addEventListener('focus', this.focusEventHandler.bind(this));
+            tab.addEventListener("focus", this.focusEventHandler.bind(this));
         }
 
         if (this.direction[pressed]) {
@@ -156,11 +155,9 @@ class TabGroup {
             if (target.index !== undefined) {
                 if (this.tabs[target.index + this.direction[pressed]]) {
                     this.tabs[target.index + this.direction[pressed]].focus();
-                }
-                else if (pressed === this.keys.left || pressed === this.keys.up) {
+                } else if (pressed === this.keys.left || pressed === this.keys.up) {
                     this.focusLastTab();
-                }
-                else if (pressed === this.keys.right || pressed == this.keys.down) {
+                } else if (pressed === this.keys.right || pressed == this.keys.down) {
                     this.focusFirstTab();
                 }
             }
@@ -168,8 +165,8 @@ class TabGroup {
     }
 
     // Activates any given tab panel
-    activateTab (tab, setFocus) {
-        if(tab.getAttribute("role") !== "tab") {
+    activateTab(tab, setFocus) {
+        if (tab.getAttribute("role") !== "tab") {
             tab = tab.closest('[role="tab"]');
         }
 
@@ -179,19 +176,19 @@ class TabGroup {
         this.deactivateTabs();
 
         // Remove tabindex attribute
-        tab.removeAttribute('tabindex');
+        tab.removeAttribute("tabindex");
 
         // Set the tab as selected
-        tab.setAttribute('aria-selected', 'true');
+        tab.setAttribute("aria-selected", "true");
 
         // Give the tab is-active class
-        tab.classList.add('is-active');
+        tab.classList.add("is-active");
 
         // Get the value of aria-controls (which is an ID)
-        var controls = tab.getAttribute('aria-controls');
+        var controls = tab.getAttribute("aria-controls");
 
         // Remove hidden attribute from tab panel to make it visible
-        document.getElementById(controls).removeAttribute('hidden');
+        document.getElementById(controls).removeAttribute("hidden");
 
         // Set focus when required
         if (setFocus) {
@@ -202,14 +199,14 @@ class TabGroup {
     // Deactivate all tabs and tab panels
     deactivateTabs() {
         for (let tab of this.tabs) {
-            tab.classList.remove('is-active');
-            tab.setAttribute('tabindex', '-1');
-            tab.setAttribute('aria-selected', 'false');
-            tab.removeEventListener('focus', this.focusEventHandler.bind(this));
+            tab.classList.remove("is-active");
+            tab.setAttribute("tabindex", "-1");
+            tab.setAttribute("aria-selected", "false");
+            tab.removeEventListener("focus", this.focusEventHandler.bind(this));
         }
 
         for (let panel of this.panels) {
-            panel.setAttribute('hidden', 'hidden');
+            panel.setAttribute("hidden", "hidden");
         }
     }
 
@@ -224,15 +221,14 @@ class TabGroup {
     // Determine whether there should be a delay
     // when user navigates with the arrow keys
     determineDelay() {
-        var hasDelay = this.tablist.hasAttribute('data-delay');
+        var hasDelay = this.tablist.hasAttribute("data-delay");
         var delay = 0;
 
         if (hasDelay) {
-            var delayValue = this.tablist.getAttribute('data-delay');
+            var delayValue = this.tablist.getAttribute("data-delay");
             if (delayValue) {
                 delay = delayValue;
-            }
-            else {
+            } else {
                 // If no value is specified, default to 300ms
                 delay = 300;
             }
