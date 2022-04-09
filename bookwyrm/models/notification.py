a2a04da49356165e53cbcd557ad2b9ad2a1e75e1
Mouse Reeve <mousereeve@riseup.net>
2022-04-09 09:41:10 -0700

    Adds many to many related items to notifications

--- bookwyrm/models/notification.py
+++ bookwyrm/models/notification.py
@@ -15,38 +15,25 @@ class Notification(BookWyrmModel):
     """you've been tagged, liked, followed, etc"""
 
     user = models.ForeignKey("User", on_delete=models.CASCADE)
-    related_user = models.ForeignKey(
-        "User", on_delete=models.CASCADE, null=True, related_name="related_user"
-    )
-    related_group = models.ForeignKey(
-        "Group", on_delete=models.CASCADE, null=True, related_name="notifications"
-    )
-    related_status = models.ForeignKey("Status", on_delete=models.CASCADE, null=True)
-    related_import = models.ForeignKey("ImportJob", on_delete=models.CASCADE, null=True)
-    related_list_item = models.ForeignKey(
-        "ListItem", on_delete=models.CASCADE, null=True
-    )
-    related_report = models.ForeignKey("Report", on_delete=models.CASCADE, null=True)
     read = models.BooleanField(default=False)
     notification_type = models.CharField(
         max_length=255, choices=NotificationType.choices
     )
 
-    def save(self, *args, **kwargs):
-        """save, but don't make dupes"""
-        # there's probably a better way to do this
-        if self.__class__.objects.filter(
-            user=self.user,
-            related_user=self.related_user,
-            related_group=self.related_group,
-            related_status=self.related_status,
-            related_import=self.related_import,
-            related_list_item=self.related_list_item,
-            related_report=self.related_report,
-            notification_type=self.notification_type,
-        ).exists():
-            return
-        super().save(*args, **kwargs)
+    related_users = models.ManyToManyField(
+        "User", symmetrical=False, related_name="notifications"
+    )
+    related_groups = models.ManyToManyField(
+        "Group", symmetrical=False, related_name="notifications"
+    )
+    related_statuses = models.ManyToManyField(
+        "Status", symmetrical=False, related_name="notifications"
+    )
+    related_import = models.ForeignKey("ImportJob", on_delete=models.CASCADE, null=True)
+    related_list_items = models.ManyToManyField(
+        "ListItem", symmetrical=False, related_name="notifications"
+    )
+    related_reports = models.ManyToManyField("Report", symmetrical=False)
 
     class Meta:
         """checks if notifcation is in enum list for valid types"""
