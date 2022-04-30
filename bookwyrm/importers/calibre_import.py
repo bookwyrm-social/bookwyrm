eeb1cc71975d98566ff9ff79f403bc3ec11cd4f0
André Jaenisch <andre.jaenisch@posteo.de>
2022-04-30 19:08:31 +0200

    Use a default shelf because Calibre indicates no reading status
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- bookwyrm/importers/calibre_import.py
+++ bookwyrm/importers/calibre_import.py
@@ -3,6 +3,10 @@ from . import Importer
 
 
 class CalibreImporter(Importer):
-    """csv downloads from OpenLibrary"""
+    """csv downloads from Calibre"""
 
     service = "Calibre"
+
+    def get_shelf(self, normalized_row):
+        # Calibre export does not indicate which shelf to use. Go with a default one for now
+        return "to-read"
