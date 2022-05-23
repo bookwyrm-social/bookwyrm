12541d5f1c46e7e074b51d19b2b8fd90b9f3e7f6
André Jaenisch <andre.jaenisch@posteo.de>
2022-05-23 20:52:26 +0200

    Map timestamp to date_added to avoid integrity error.
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- bookwyrm/importers/calibre_import.py
+++ bookwyrm/importers/calibre_import.py
@@ -9,6 +9,20 @@ class CalibreImporter(Importer):
 
     service = "Calibre"
 
+    def __init__(self, *args, **kwargs):
+        # Add timestamp to row_mappings_guesses for date_added to avoid
+        # integrity error
+        row_mappings_guesses = []
+
+        for field, mapping in self.row_mappings_guesses:
+            if field in ('date_added',):
+                row_mappings_guesses.append((field, mapping + ['timestamp']))
+            else:
+                row_mappings_guesses.append((field, mapping))
+
+        self.row_mappings_guesses = row_mappings_guesses
+        super().__init__(*args, **kwargs)
+
     def get_shelf(self, normalized_row):
         # Calibre export does not indicate which shelf to use. Go with a default one for now
         return Shelf.TO_READ
