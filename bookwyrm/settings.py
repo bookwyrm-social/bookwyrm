4e0e6ed5a4237b7e122cd34355f7367c086928c1
Mouse Reeve <mousereeve@riseup.net>
2022-05-26 12:48:24 -0700

    Tick javascript cache and version number

--- bookwyrm/settings.py
+++ bookwyrm/settings.py
@@ -11,7 +11,7 @@ from django.utils.translation import gettext_lazy as _
 env = Env()
 env.read_env()
 DOMAIN = env("DOMAIN")
-VERSION = "0.3.4"
+VERSION = "0.4.0"
 
 RELEASE_API = env(
     "RELEASE_API",
@@ -21,7 +21,7 @@ RELEASE_API = env(
 PAGE_LENGTH = env("PAGE_LENGTH", 15)
 DEFAULT_LANGUAGE = env("DEFAULT_LANGUAGE", "English")
 
-JS_CACHE = "bc93172a"
+JS_CACHE = "e678183b"
 
 # email
 EMAIL_BACKEND = env("EMAIL_BACKEND", "django.core.mail.backends.smtp.EmailBackend")
