7014786fe03a73c7c8fe525aaad19384aa05c159
Joel Bradshaw <cincodenada@gmail.com>
2022-06-05 13:41:00 -0700

    Run formatters

--- bookwyrm/utils/log.py
+++ bookwyrm/utils/log.py
@@ -3,10 +3,10 @@ import logging
 
 class IgnoreVariableDoesNotExist(logging.Filter):
     def filter(self, record):
-        if(record.exc_info):
+        if record.exc_info:
             (errType, errValue, _) = record.exc_info
             while errValue:
-                if type(errValue).__name__ == 'VariableDoesNotExist':
+                if type(errValue).__name__ == "VariableDoesNotExist":
                     return False
                 errValue = errValue.__context__
             return True
