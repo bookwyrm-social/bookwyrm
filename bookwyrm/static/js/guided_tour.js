57965973dc075817dbcce69d1815d4d8bc8592b7
Hugh Rundle <hugh@hughrundle.net>
2022-06-18 13:27:00 +1000

    make guided tour cancel button DRY
    Move cancel button function into a separate JS file.
    The selector JS for this function cannot be within bookwyrm.js because the guided tour elements load after bookwyrm.js.

---
+++ bookwyrm/static/js/guided_tour.js
@@ -0,0 +1,17 @@
+/**
+ * Set guided tour user value to False
+ * @param  {csrf_token} string
+ * @return {undefined}
+ */
+
+function disableGuidedTour(csrf_token) {
+    console.log(csrf_token);
+    fetch('/guided-tour/False', {
+        headers: {
+            'X-CSRFToken': csrf_token,
+        },
+        method: 'POST', 
+        redirect: 'follow',
+        mode: 'same-origin',
+    })
+}
