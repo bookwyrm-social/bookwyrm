079f6d304e0d6ad89da4be94a6f48d79f6c25004
Mouse Reeve <mousereeve@riseup.net>
2022-07-06 12:22:07 -0700

    Python formatting

--- bookwyrm/migrations/0152_alter_report_user.py
+++ bookwyrm/migrations/0152_alter_report_user.py
@@ -8,13 +8,18 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('bookwyrm', '0151_alter_report_user'),
+        ("bookwyrm", "0151_alter_report_user"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='report',
-            name='user',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.PROTECT, to=settings.AUTH_USER_MODEL),
+            model_name="report",
+            name="user",
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.PROTECT,
+                to=settings.AUTH_USER_MODEL,
+            ),
         ),
     ]
