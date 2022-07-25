8b9dc0dd4af6d435eb9dc5a18910c07fc49f1ec1
Pedromex2007 <34046361+Pedromex2007@users.noreply.github.com>
2022-07-25 13:13:16 -0400

    Add experimental multi-select list

--- bookwyrm/views/search_genre.py
+++ bookwyrm/views/search_genre.py
@@ -1,4 +1,9 @@
 from django.contrib.postgres.search import TrigramSimilarity
+
+from django.shortcuts import get_object_or_404, render
+
+from bookwyrm.models.book import Genre
+
 from django.core.paginator import Paginator
 from django.db.models.functions import Greatest
 from django.http import JsonResponse
@@ -10,3 +15,20 @@ from django.views.generic import (
 
 class SearchGenre(TemplateView):
     template_name = 'search/genre_search.html'
+
+    def post(self, request, *args, **kwargs):
+        testList = request.POST.getlist('genres')
+
+        for item in testList:
+            print("Item successful captured!")
+            print(item)
+
+        context = self.get_context_data()
+        return render(request, self.template_name, context)
+
+    def get_context_data(self,*args, **kwargs):
+        context = super(SearchGenre, self).get_context_data(*args,**kwargs)
+        context['genre_tags'] = Genre.objects.all()
+        return context
+
+    
