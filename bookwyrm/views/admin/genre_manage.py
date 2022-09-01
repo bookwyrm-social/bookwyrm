6700431d51ac2bd78d53b1749aac057cd54528ec
Pedromex2007 <34046361+Pedromex2007@users.noreply.github.com>
2022-09-01 15:55:45 -0400

    Stylize genre add page

--- bookwyrm/views/admin/genre_manage.py
+++ bookwyrm/views/admin/genre_manage.py
@@ -3,6 +3,7 @@ from django.contrib.postgres.search import TrigramSimilarity
 from django.shortcuts import get_object_or_404, render
 
 from bookwyrm.models.book import Genre, Book
+from bookwyrm.forms import GenreForm
 
 from django.urls import reverse_lazy
 from django.core.paginator import Paginator
@@ -31,8 +32,10 @@ class ModifyGenre(UpdateView):
 class CreateGenre(CreateView):
     template_name = 'settings/genres/genre_add.html'
     model = Genre
+    form_class = GenreForm
     success_url = reverse_lazy('settings-genres')
-    fields = ['genre_name', 'description']
+    #fields = ['genre_name', 'description']
+
 
 class RemoveGenre(DeleteView):
     template_name = 'settings/genres/genre_delete.html'
