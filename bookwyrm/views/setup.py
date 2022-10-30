488a4026bd95ba43450cb7bc9de4b3aff705f7d4
McKerlton Hyppolite <Mchype99@gmail.com>
2022-10-30 16:22:10 -0400

    Update setup.py

--- bookwyrm/views/setup.py
+++ bookwyrm/views/setup.py
@@ -107,7 +107,7 @@ class CreateAdmin(View):
     def create_default_genres(self, request):
         """Create the initial genres. Can be changed, of course."""
         for genre_name in DEFAULT_GENRES:
-            action_genre = models.Genre.objects.create_genre(
+            genre = models.Genre.objects.create_genre(
                 genre_name, DEFAULT_GENRES[genre_name]
             )
-            action_genre.save()
+            genre.save()
