5abe4e887fd38fd416ef7ebd749e67fb52377d89
Mouse Reeve <mousereeve@riseup.net>
2022-11-01 17:26:40 -0700

    Use generated path to login url
    This is safer; if the url changes for whatever reason, the link won't
    break.

--- bookwyrm/settings.py
+++ bookwyrm/settings.py
@@ -21,7 +21,7 @@ RELEASE_API = env(
 PAGE_LENGTH = env("PAGE_LENGTH", 15)
 DEFAULT_LANGUAGE = env("DEFAULT_LANGUAGE", "English")
 
-JS_CACHE = "e678183b"
+JS_CACHE = "e678183c"
 
 # email
 EMAIL_BACKEND = env("EMAIL_BACKEND", "django.core.mail.backends.smtp.EmailBackend")
