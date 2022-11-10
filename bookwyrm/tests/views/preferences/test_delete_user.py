eae1866992b30e5aa7f31f2561a9121d21ce087d
Mouse Reeve <mousereeve@riseup.net>
2022-11-10 13:40:54 -0800

    Allow users to temporarily deactivate their accounts (#2324)

--- bookwyrm/tests/views/preferences/test_delete_user.py
+++ bookwyrm/tests/views/preferences/test_delete_user.py
@@ -2,6 +2,7 @@
 import json
 from unittest.mock import patch
 
+from django.contrib.auth.models import AnonymousUser
 from django.contrib.sessions.middleware import SessionMiddleware
 from django.template.response import TemplateResponse
 from django.test import TestCase
@@ -15,6 +16,7 @@ from bookwyrm.tests.validate_html import validate_html
 class DeleteUserViews(TestCase):
     """view user and edit profile"""
 
+    # pylint: disable=invalid-name
     def setUp(self):
         """we need basic test data and mocks"""
         self.factory = RequestFactory()
@@ -22,14 +24,18 @@ class DeleteUserViews(TestCase):
             "bookwyrm.activitystreams.populate_stream_task.delay"
         ), patch("bookwyrm.lists_stream.populate_lists_task.delay"):
             self.local_user = models.User.objects.create_user(
-                "mouse@local.com",
+                "mouse@your.domain.here",
                 "mouse@mouse.mouse",
                 "password",
                 local=True,
                 localname="mouse",
             )
             self.rat = models.User.objects.create_user(
-                "rat@local.com", "rat@rat.rat", "password", local=True, localname="rat"
+                "rat@your.domain.here",
+                "rat@rat.rat",
+                "password",
+                local=True,
+                localname="rat",
             )
 
             self.book = models.Edition.objects.create(
@@ -44,6 +50,8 @@ class DeleteUserViews(TestCase):
                     shelf=self.local_user.shelf_set.first(),
                 )
 
+        self.anonymous_user = AnonymousUser
+        self.anonymous_user.is_authenticated = False
         models.SiteSettings.objects.create()
 
     def test_delete_user_page(self, _):
@@ -84,3 +92,52 @@ class DeleteUserViews(TestCase):
         self.local_user.refresh_from_db()
         self.assertFalse(self.local_user.is_active)
         self.assertEqual(self.local_user.deactivation_reason, "self_deletion")
+
+    def test_deactivate_user(self, _):
+        """Impermanent deletion"""
+        self.assertTrue(self.local_user.is_active)
+        view = views.DeactivateUser.as_view()
+        request = self.factory.post("")
+        request.user = self.local_user
+        middleware = SessionMiddleware()
+        middleware.process_request(request)
+        request.session.save()
+
+        view(request)
+
+        self.local_user.refresh_from_db()
+        self.assertFalse(self.local_user.is_active)
+        self.assertEqual(self.local_user.deactivation_reason, "self_deactivation")
+
+    def test_reactivate_user_get(self, _):
+        """Reactication page"""
+        view = views.ReactivateUser.as_view()
+        request = self.factory.get("")
+        request.user = self.anonymous_user
+
+        result = view(request)
+        self.assertIsInstance(result, TemplateResponse)
+        validate_html(result.render())
+        self.assertEqual(result.status_code, 200)
+
+    def test_reactivate_user_post(self, _):
+        """Reactivate action"""
+        self.local_user.deactivate()
+        self.local_user.refresh_from_db()
+
+        view = views.ReactivateUser.as_view()
+        form = forms.LoginForm()
+        form.data["localname"] = "mouse"
+        form.data["password"] = "password"
+        request = self.factory.post("", form.data)
+        request.user = self.local_user
+        middleware = SessionMiddleware()
+        middleware.process_request(request)
+        request.session.save()
+
+        with patch("bookwyrm.views.preferences.delete_user.login"):
+            view(request)
+
+        self.local_user.refresh_from_db()
+        self.assertTrue(self.local_user.is_active)
+        self.assertIsNone(self.local_user.deactivation_reason)
