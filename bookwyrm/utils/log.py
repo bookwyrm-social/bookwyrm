c9adb7ff129bdb75fcbe97c896347cfb889683a0
Ell Bradshaw <ell@cincodenada.com>
2022-11-14 00:48:59 -0800

    Linting fixes

--- bookwyrm/utils/log.py
+++ bookwyrm/utils/log.py
@@ -1,12 +1,20 @@
+""" Logging utilities """
 import logging
 
 
 class IgnoreVariableDoesNotExist(logging.Filter):
+    """
+    Filter to ignore VariableDoesNotExist errors
+
+    We intentionally pass nonexistent variables to templates a lot, so
+    these errors are not useful to us.
+    """
+
     def filter(self, record):
         if record.exc_info:
-            (errType, errValue, _) = record.exc_info
-            while errValue:
-                if type(errValue).__name__ == "VariableDoesNotExist":
+            (_, err_value, _) = record.exc_info
+            while err_value:
+                if type(err_value).__name__ == "VariableDoesNotExist":
                     return False
-                errValue = errValue.__context__
-            return True
+                err_value = err_value.__context__
+        return True
