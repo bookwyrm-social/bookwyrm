ac2ef667aff282e8273c92ec339a011d41c51aa2
Joachim <joachim.robert@protonmail.com>
2023-01-01 19:57:10 +0100

    Fix error when no referer

--- bookwyrm/utils/validate.py
+++ bookwyrm/utils/validate.py
@@ -4,7 +4,7 @@ from bookwyrm.settings import DOMAIN, USE_HTTPS
 
 def validate_url_domain(url, default="/"):
     """Basic check that the URL starts with the instance domain name"""
-    if url in ("/", default):
+    if url in ("/", default, None):
         return url
 
     protocol = "https://" if USE_HTTPS else "http://"
