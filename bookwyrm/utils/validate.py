fa3edea87b9dc23ae6c714aa3f42cb26b6023d8d
Joachim <joachim.robert@protonmail.com>
2023-01-01 20:51:23 +0100

    Update validate.py

--- bookwyrm/utils/validate.py
+++ bookwyrm/utils/validate.py
@@ -4,7 +4,10 @@ from bookwyrm.settings import DOMAIN, USE_HTTPS
 
 def validate_url_domain(url, default="/"):
     """Basic check that the URL starts with the instance domain name"""
-    if url in ("/", default, None):
+    if not url:
+        return default
+
+    if url in ("/", default):
         return url
 
     protocol = "https://" if USE_HTTPS else "http://"
