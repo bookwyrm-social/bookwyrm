c450947eeee26727dd48a5dd63c42d6ed45bc889
Hugh Rundle <hugh@hughrundle.net>
2023-04-11 18:57:55 +1000

    update comment to identify bug

--- bookwyrm/models/status.py
+++ bookwyrm/models/status.py
@@ -137,9 +137,9 @@ class Status(OrderedCollectionPageMixin, BookWyrmModel):
         if activity.tag == MISSING or activity.tag is None:
             return True
 
-        # BUG: this fixes the TypeError but we still don't get any notifs
-        # and DMs are dropped
-        tags = activity.tag if type(activity.tag) == list else [activity.tag]
+        # BUG: this fixes the TypeError but if there is only one user mentioned
+        # we still don't get any notifs and DMs are dropped.
+        tags = activity.tag if type(activity.tag) == list else [ activity.tag ]
         user_model = apps.get_model("bookwyrm.User", require_ready=True)
         for tag in tags:
             if (
