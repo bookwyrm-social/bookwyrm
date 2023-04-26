b77ae9e7833f005e13e0a0d9c329c8ab6718cca0
Mouse Reeve <mousereeve@riseup.net>
2023-04-25 17:42:32 -0700

    Ticks version number

--- bookwyrm/settings.py
+++ bookwyrm/settings.py
@@ -12,7 +12,7 @@ from django.core.exceptions import ImproperlyConfigured
 env = Env()
 env.read_env()
 DOMAIN = env("DOMAIN")
-VERSION = "0.6.1"
+VERSION = "0.6.2"
 
 RELEASE_API = env(
     "RELEASE_API",
@@ -22,7 +22,7 @@ RELEASE_API = env(
 PAGE_LENGTH = env.int("PAGE_LENGTH", 15)
 DEFAULT_LANGUAGE = env("DEFAULT_LANGUAGE", "English")
 
-JS_CACHE = "a7d4e720"
+JS_CACHE = "ea91d7df"
 
 # email
 EMAIL_BACKEND = env("EMAIL_BACKEND", "django.core.mail.backends.smtp.EmailBackend")
