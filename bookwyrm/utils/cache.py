af5f71f5acdb2a8537f86220e97500b4ae568108
Joeri de Ruiter <jderuiter@users.noreply.github.com>
2023-09-13 10:21:30 +0200

    Revert return type for get_or_set

--- bookwyrm/utils/cache.py
+++ bookwyrm/utils/cache.py
@@ -6,12 +6,12 @@ from django.core.cache import cache
 
 def get_or_set(
     cache_key: str,
-    function: Callable[..., str],
+    function: Callable[..., Any],
     *args: Tuple[Any, ...],
     timeout: Union[float, None] = None
-) -> str:
+) -> Any:
     """Django's built-in get_or_set isn't cutting it"""
-    value = str(cache.get(cache_key))
+    value = cache.get(cache_key)
     if value is None:
         value = function(*args)
         cache.set(cache_key, value, timeout=timeout)
