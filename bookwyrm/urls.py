e144ce19fa9021f882952b3e87c6852812400b83
Sean Molenaar <SMillerDev@users.noreply.github.com>
2023-11-16 10:48:06 +0100

    fix: add include import from django.urls

--- bookwyrm/urls.py
+++ bookwyrm/urls.py
@@ -2,7 +2,7 @@
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from django.views.generic.base import TemplateView
 
 from bookwyrm import settings, views
