ea9d3f8ba1ac3db3a050b0355bd3de3d20cd061e
Bart Schuurmans <bart@minnozz.com>
2024-01-20 17:25:20 +0100

    Use Status.page_image for OpenGraph tags

--- bookwyrm/views/feed.py
+++ bookwyrm/views/feed.py
@@ -185,12 +185,6 @@ class Status(View):
             params=[status.id, visible_thread, visible_thread],
         )
 
-        preview = None
-        if hasattr(status, "book"):
-            preview = status.book.preview_image
-        elif status.mention_books.exists():
-            preview = status.mention_books.first().preview_image
-
         data = {
             **feed_page_data(request.user),
             **{
@@ -199,7 +193,7 @@ class Status(View):
                 "ancestors": ancestors,
                 "title": status.page_title,
                 "description": status.page_description,
-                "preview": preview,
+                "page_image": status.page_image,
             },
         }
         return TemplateResponse(request, "feed/status.html", data)
