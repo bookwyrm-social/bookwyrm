93396c0f2cefacee6d1c53ee56c6f14b2a7c0882
Dato Simó <dato@net.com.org.es>
2024-09-01 02:21:10 -0300

    Use typing.ParamSpec to type get_or_set()

--- bookwyrm/utils/cache.py
+++ bookwyrm/utils/cache.py
@@ -1,17 +1,20 @@
 """ Custom handler for caching """
-from typing import Any, Callable, Tuple, Union
+from typing import Callable, Optional, ParamSpec, TypeVar, cast
 
 from django.core.cache import cache
+
+Args = ParamSpec("Args")
+Ret = TypeVar("Ret")
 
 
 def get_or_set(
     cache_key: str,
-    function: Callable[..., Any],
-    *args: Tuple[Any, ...],
-    timeout: Union[float, None] = None
-) -> Any:
+    function: Callable[Args, Ret],
+    *args: Args.args,
+    timeout: Optional[float] = None
+) -> Ret:
     """Django's built-in get_or_set isn't cutting it"""
-    value = cache.get(cache_key)
+    value = cast(Optional[Ret], cache.get(cache_key))
     if value is None:
         value = function(*args)
         cache.set(cache_key, value, timeout=timeout)
