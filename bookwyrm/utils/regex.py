c9406c40a4d86e728bf9e8a8a45679017cca8ab1
Tim Rogers <rogers.timothy.john@gmail.com>
2025-03-30 09:45:23 -0500

    Split long REMOTENAME regex into multiline string for readability

--- bookwyrm/utils/regex.py
+++ bookwyrm/utils/regex.py
@@ -3,7 +3,10 @@
 DOMAIN = r"[\w_\-\.]+\.[a-z\-]{2,}"
 LOCALNAME = r"@?[a-zA-Z_\-\.0-9]+"
 STRICT_LOCALNAME = r"@[a-zA-Z_\-\.0-9]+"
-REMOTENAME = r"[\w\-\.\~\!\$\&\'\(\)\*\+\,\;\=](?:[\w\-\.\~\!\$\&\'\(\)\*\+\,\;\=]|(?:%[0-9A-Fa-f]{2})){0,149}"
+REMOTENAME = (
+    r"[\w\-\.\~\!\$\&\'\(\)\*\+\,\;\=]"
+    r"(?:[\w\-\.\~\!\$\&\'\(\)\*\+\,\;\=]|(?:%[0-9A-Fa-f]{2})){0,149}"
+)
 USERNAME = rf"{LOCALNAME}(@{DOMAIN})?"
 STRICT_USERNAME = rf"(\B{STRICT_LOCALNAME}(@{DOMAIN})?\b)"
 FULL_USERNAME = rf"{LOCALNAME}@{DOMAIN}\b"
