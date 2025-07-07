780c296037a81db19943bfa9025f30bdc9e2efa6
Tim Rogers <rogers.timothy.john@gmail.com>
2025-07-07 08:51:29 -0500

    Disabled too many public methods on group model tests

--- bookwyrm/tests/views/test_group.py
+++ bookwyrm/tests/views/test_group.py
@@ -11,7 +11,7 @@ from django.test.client import RequestFactory
 from bookwyrm import models, views
 from bookwyrm.tests.validate_html import validate_html
 
-
+# pylint: disable=too-many-public-methods
 @patch("bookwyrm.models.activitypub_mixin.broadcast_task.apply_async")
 class GroupViews(TestCase):
     """view group and edit details"""
