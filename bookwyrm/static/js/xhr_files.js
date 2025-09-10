6df4484b5cabd13de7322ef8a04a626294c3cc57
Ian Young <ian@iangreenleaf.com>
2025-09-09 22:35:07 -0500

    Insert image in text after uploading

--- bookwyrm/static/js/xhr_files.js
+++ bookwyrm/static/js/xhr_files.js
@@ -23,38 +23,32 @@ let XhrFiles = new (class {
      * @return {undefined}
      */
     dropFile(event) {
-        console.log(event);
         event.preventDefault();
 
-        let result = "";
         // Use DataTransferItemList interface to access the file(s)
         [...event.dataTransfer.items].forEach((item, i) => {
             // If dropped items aren't files, reject them
             if (item.kind === "file") {
                 const file = item.getAsFile();
-                this.uploadFile(file);
-                result += `• file[${i}].name = ${file.name}\n`;
+                this.uploadFile(file, event.target);
             }
         });
-        console.log(result);
     }
 
-    uploadFile(file) {
-        var xhr = new XMLHttpRequest();
-        (xhr.upload || xhr).addEventListener('progress', function(e) {
-            var done = e.position || e.loaded
-            var total = e.totalSize || e.total;
-            console.log('xhr progress: ' + Math.round(done/total*100) + '%');
-        });
+    uploadFile(file, target) {
+        const self = this;
+        const xhr = new XMLHttpRequest();
         xhr.addEventListener('load', function(e) {
             if (this.status != 200) {
                 console.log(e);
                 return;
             }
-            console.log(e);
+            const responseJSON = JSON.parse(e.target.responseText);
+            const url = responseJSON.url;
+            self.insertImageMarkdown(target, file, url);
         });
         xhr.open('post', '/upload', true);
-        var fd = new FormData();
+        const fd = new FormData();
         fd.append("filename", file.name);
         fd.append("file", file);
         fd.append("csrfmiddlewaretoken", this.csrfToken());
@@ -64,5 +58,14 @@ let XhrFiles = new (class {
 
     csrfToken() {
         return document.querySelector('[name=csrfmiddlewaretoken]').value;
+    }
+
+    insertImageMarkdown(target, file, url) {
+        const imageMarkdown = `![${file.name}](${url})`;
+        const content = target.value;
+        const caret = target.selectionEnd;
+        const preCaret = content.slice(0, caret);
+        const postCaret = content.slice(caret);
+        target.value = preCaret.concat(imageMarkdown, postCaret);
     }
 })();
