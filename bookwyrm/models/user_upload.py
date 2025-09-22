afecfb2f5851ba5215a274af74b07b67b78ea489
Ian Young <ian@iangreenleaf.com>
2025-09-21 21:29:32 -0500

    Create multiple sizes of image on each upload

--- bookwyrm/models/user_upload.py
+++ bookwyrm/models/user_upload.py
@@ -1,16 +1,31 @@
 """ database schema for images uploaded by users """
 from django.db import models
 
-def user_directory_path(instance, filename):
-    return "uploads/user_{0}/{1}".format(instance.user.id, filename)
+def user_upload_directory_path(instance, filename):
+    return "uploads/user_{0}/{1}/{2}".format(instance.user.id, instance.id, filename)
 
 class UserUpload(models.Model):
     original_name = models.TextField()
     original_content_type = models.TextField()
-    file = models.ImageField(upload_to=user_directory_path)
+    original_file = models.ImageField(upload_to=user_upload_directory_path)
     user = models.ForeignKey(
         "User",
         on_delete=models.CASCADE,
         null=False,
         blank=False,
+        related_name="user_uploads"
+    )
+
+def user_upload_version_directory_path(instance, filename):
+    return "uploads/user_{0}/{1}/{2}".format(instance.user_upload.user.id, instance.user_upload.id, instance.max_dimension)
+
+class UserUploadVersion(models.Model):
+    max_dimension = models.TextField()
+    file = models.ImageField(upload_to=user_upload_version_directory_path)
+    user_upload = models.ForeignKey(
+        "UserUpload",
+        on_delete=models.CASCADE,
+        null=False,
+        blank=False,
+        related_name="versions"
     )
--- bookwyrm/views/user_upload.py
+++ bookwyrm/views/user_upload.py
@@ -1,6 +1,11 @@
 import re
 import logging
+import tempfile
+from PIL import Image
+from environs import Env
+from io import BytesIO
 
+from django.core.files import File
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseBadRequest, Http404
 from django.utils.decorators import method_decorator
@@ -18,11 +23,47 @@ class CreateUserUpload(View):
         file = request.FILES['file']
         upload = models.UserUpload(
                     user=request.user,
-                    file=file,
                     original_name=file.name,
-                    original_content_type=file.content_type
+                    original_content_type=file.content_type,
+                    original_file=file
                 )
         upload.save()
+
+        image = Image.open(file)
+        width, height = image.size
+        env = Env()
+        sizes = env.list("UPLOAD_IMAGE_SIZES", [400, 1200], subcast=int)
+
+        for size in sizes:
+            v = self.create_version(image, upload, size)
+            if width < size and height < size:
+                break
+
+        biggest = upload.versions.order_by("-max_dimension")[0]
         return JsonResponse({
-                "url": request.build_absolute_uri(upload.file.url)
-            })
+                "url": request.build_absolute_uri(biggest.file.url)
+            }, status = 201)
+
+    def create_version(self, image, user_upload, max_dimension):
+        image_format = image.format
+        width, height = image.size
+        target_width, target_height = target_size(width, height, max_dimension)
+        if target_width != width or target_height != height:
+            image = image.resize([target_width, target_height])
+
+        img_byte_arr = BytesIO()
+        image.save(img_byte_arr, image_format)
+
+        return user_upload.versions.create(
+                    max_dimension=max_dimension,
+                    user_upload=user_upload,
+                    file=File(img_byte_arr, name = "resized")
+                )
+
+def target_size(width, height, max_dimension):
+    if width < max_dimension and height < max_dimension:
+        return [width, height]
+    elif width > height:
+        return [max_dimension, round(height * (max_dimension / width))]
+    else:
+        return [round(width * (max_dimension / height)), max_dimension]
