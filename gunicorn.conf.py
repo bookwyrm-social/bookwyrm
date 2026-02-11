b5739ca523a08ec15a9b512d2ef604e8960bb695
Ilkka Ollakka <ilkka.ollakka@iki.fi>
2026-02-11 15:05:12 +0200

    gunicorn: pump version and limit workers to be 1-12
    https://gunicorn.org/design/?h=worker#how-many-workers mentions that 4-12 should be enough for heavy
    traffic, so limit worker amount default to be at max 12.

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -5,6 +5,8 @@ env = Env()
 env.read_env()
 
 bind = "0.0.0.0:8000"
-workers = multiprocessing.cpu_count() * 2 + 1
+# clip worker count to be between 1 and 12
+#  https://gunicorn.org/design/?h=worker#how-many-workers
+workers = min([multiprocessing.cpu_count() * 2 + 1, 12])
 if env.bool("DEBUG", False):
     reload = True
