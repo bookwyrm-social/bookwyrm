8bc4d0d51bebe3cf8bfbcb8fd387c6f162f7636a
Ian Young <ian@iangreenleaf.com>
2026-02-21 21:49:24 -0600

    Fix JS lint violations

--- bookwyrm/static/js/xhr_files.js
+++ bookwyrm/static/js/xhr_files.js
@@ -1,5 +1,4 @@
 /* exported XhrFiles */
-/* globals BookWyrm */
 
 let XhrFiles = new (class {
     constructor() {
@@ -7,9 +6,13 @@ let XhrFiles = new (class {
     }
 
     initEventListeners() {
-        window.addEventListener("dragover", (e) => { e.preventDefault(); });
+        window.addEventListener("dragover", (e) => {
+            e.preventDefault();
+        });
 
-        window.addEventListener("drop", (e) => { e.preventDefault(); });
+        window.addEventListener("drop", (e) => {
+            e.preventDefault();
+        });
 
         document
             .querySelectorAll("[data-droppable-textfield]")
@@ -26,10 +29,11 @@ let XhrFiles = new (class {
         event.preventDefault();
 
         // Use DataTransferItemList interface to access the file(s)
-        [...event.dataTransfer.items].forEach((item, i) => {
+        [...event.dataTransfer.items].forEach((item) => {
             // If dropped items aren't files, reject them
             if (item.kind === "file") {
                 const file = item.getAsFile();
+
                 this.uploadFile(file, event.target);
             }
         });
@@ -38,16 +42,20 @@ let XhrFiles = new (class {
     uploadFile(file, target) {
         const self = this;
         const xhr = new XMLHttpRequest();
-        xhr.addEventListener('load', function(e) {
+
+        xhr.addEventListener("load", function (e) {
             if (this.status != 201) {
                 console.log(e);
+
                 return;
             }
             const responseJSON = JSON.parse(e.target.responseText);
+
             self.insertImageMarkdown(target, file, responseJSON);
         });
-        xhr.open('post', '/upload', true);
+        xhr.open("post", "/upload", true);
         const fd = new FormData();
+
         fd.append("filename", file.name);
         fd.append("file", file);
         fd.append("csrfmiddlewaretoken", this.csrfToken());
@@ -56,7 +64,7 @@ let XhrFiles = new (class {
     }
 
     csrfToken() {
-        return document.querySelector('[name=csrfmiddlewaretoken]').value;
+        return document.querySelector("[name=csrfmiddlewaretoken]").value;
     }
 
     insertImageMarkdown(target, file, metadata) {
@@ -65,6 +73,7 @@ let XhrFiles = new (class {
         const caret = target.selectionEnd;
         const preCaret = content.slice(0, caret);
         const postCaret = content.slice(caret);
+
         target.value = preCaret.concat(imageMarkdown, postCaret);
     }
 })();
