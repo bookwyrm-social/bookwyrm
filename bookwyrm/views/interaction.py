30b2ab8d67871de1d9326ce6c044f601c71b1a4e
Mouse Reeve <mouse.reeve@gmail.com>
2026-06-16 10:39:11 -0700

    Changes a handful of redirects to redirect_to_referer

--- bookwyrm/views/interaction.py
+++ bookwyrm/views/interaction.py
@@ -9,7 +9,7 @@ from django.utils.decorators import method_decorator
 from django.views import View
 
 from bookwyrm import models
-from .helpers import is_api_request
+from .helpers import is_api_request, redirect_to_referer
 
 
 @method_decorator(login_required, name="dispatch")
@@ -28,7 +28,7 @@ class Favorite(View):
 
         if is_api_request(request):
             return HttpResponse()
-        return redirect("/")
+        return redirect_to_referer(request)
 
 
 @method_decorator(login_required, name="dispatch")
@@ -48,7 +48,7 @@ class Unfavorite(View):
         favorite.delete()
         if is_api_request(request):
             return HttpResponse()
-        return redirect("/")
+        return redirect_to_referer(request)
 
 
 @method_decorator(login_required, name="dispatch")
@@ -67,7 +67,7 @@ class Boost(View):
             boosted_status=status, user=request.user
         ).exists():
             # you already boosted that.
-            return redirect("/")
+            return redirect_to_referer(request)
 
         models.Boost.objects.create(
             boosted_status=status,
@@ -76,7 +76,7 @@ class Boost(View):
         )
         if is_api_request(request):
             return HttpResponse()
-        return redirect("/")
+        return redirect_to_referer(request)
 
 
 @method_decorator(login_required, name="dispatch")
@@ -94,4 +94,4 @@ class Unboost(View):
         boost.delete()
         if is_api_request(request):
             return HttpResponse()
-        return redirect("/")
+        return redirect_to_referer(request)
