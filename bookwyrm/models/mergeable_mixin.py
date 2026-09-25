a2d6583437c18aa9d73ca3f38a98477224700049
Mouse Reeve <mouse.reeve@gmail.com>
2026-09-25 07:26:13 -0700

    Only clear work target if it's linking to itself

--- bookwyrm/models/mergeable_mixin.py
+++ bookwyrm/models/mergeable_mixin.py
@@ -245,7 +245,8 @@ class MergeableMixin(models.Model):
         ):
             parent.merge_into(canonical.parent_work)
             # make sure we don't create a situation where the work is trying to merge into itself
-            canonical.parent_work.pending_merge_target = None
-            canonical.parent_work.save(
-                update_fields=["pending_merge_target"], broadcast=False
-            )
+            if canonical.parent_work == canonical:
+                canonical.parent_work.pending_merge_target = None
+                canonical.parent_work.save(
+                    update_fields=["pending_merge_target"], broadcast=False
+                )
